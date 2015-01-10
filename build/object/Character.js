7d3b7aff1bf8f307af17313f639d508c5fd862fc
Pontus Persson <pom@spotify.com>
2015-01-10 19:39:18 +0100

    Fix line endings in Character.

--- build/object/Character.js
+++ build/object/Character.js
@@ -1,20 +1,20 @@
 Engine.assets.objects.Character = function()
 {
-	this.__proto__ = new Engine.assets.Object();
-	var self = this;
-	self.health = new Engine.assets.Energy(100);
-	self.direction = undefined;
-	self.setGravity(10);
+    this.__proto__ = new Engine.assets.Object();
+    var self = this;
+    self.health = new Engine.assets.Energy(100);
+    self.direction = undefined;
+    self.setGravity(10);
 
-	self.setDirection = function(d)
-	{
-		self.direction = d;
-	}
+    self.setDirection = function(d)
+    {
+        self.direction = d;
+    }
 
-	self.timeShift = function(t)
-	{
-		self.__proto__.timeShift(t);
-	}
+    self.timeShift = function(t)
+    {
+        self.__proto__.timeShift(t);
+    }
 }
 
 Engine.assets.objects.characters = {};
