61b3c5463b88f92a20a8d42aa59e4f11bdb4887c
Pontus Persson <pom@spotify.com>
2015-01-15 00:38:34 +0100

    Improve test suite.

--- test/test.js
+++ test/test.js
@@ -8,20 +8,25 @@ var Test = function()
 
 	this.run = function(log) {
 		var i, j;
+		var tests = 0;
+		var assertions = 0;
 		for (i in this.suites) {
 			var suite = this.suites[i];
 			suite.output = log;
+			suite.assertions = 0;
 			for (j in suite.tests) {
 				var test = suite.tests[j];
 				try {
 					log.value += '.';
 					test.callback.call(suite);
+					tests++;
 				} catch (e) {
-					log.value += "\n" + suite.name + ':' + test.name + ' failed: ' + e.message + "\n";
-					throw e;
+					log.value += "\n" + suite.name + ':' + test.name + ' failed: ' + e.stack + "\n";
 				}
 			}
+			assertions += suite.assertions;
 		}
+		log.value += "\n" + tests + ' tests, ' + assertions + ' assertions ' + "\n";
 	}
 }
 
@@ -37,22 +42,29 @@ Test.Suite = function(name)
 		});
 	}
 
-	this.assertEquals = function(v1, v2, m) {
-		this.assertions += 1;
+	this.assertEquals = function(v2, v1, m) {
+		this.assertions++;
 		if (v1 !== v2) {
-			throw new Error('Failed to assert that ' + v1 + ' equals ' + v2);
+			throw new Error('Failed to assert that ' + v1 + ' equals expected ' + v2);
+		}
+	}
+
+	this.assertEqualFloats = function(v2, v1, m) {
+		this.assertions++;
+		if (v1.toFixed(10) !== v2.toFixed(10)) {
+			throw new Error('Failed to assert that ' + v1 + ' float close to ' + v2);
 		}
 	}
 
 	this.log = function(text) {
 		this.output.value += text;
 	}
-
 }
 
 var test = new Test();
 var suite = new Test.Suite('Engine.UVAnimator');
-suite.add('testFrames', function() {
+suite.add('testInfiniteTime', function() {
+
 	var anim = new Engine.UVAnimator(new THREE.Geometry());
 	anim.addFrame({}, 1);
 	anim.addFrame({}, 4);
@@ -63,18 +75,39 @@ suite.add('testFrames', function() {
 	this.assertEquals(0, anim.infiniteTime);
 	var len = anim.totalDuration;
 
+	anim.timeShift(-3);
+	this.assertEquals(4, anim.infiniteTime);
 	anim.timeShift(5);
-	this.assertEquals(5, anim.infiniteTime);
-	anim.timeShift(5);
-	this.assertEquals(3, anim.infiniteTime);
+	this.assertEquals(2, anim.infiniteTime);
 
+	// Big numbers
 	anim.timeShift(len * -4933);
-	this.assertEquals(3, anim.infiniteTime);
+	this.assertEquals(2, anim.infiniteTime);
 	anim.timeShift(len * 6823);
-	this.assertEquals(3, anim.infiniteTime);
+	this.assertEquals(2, anim.infiniteTime);
 	anim.timeShift((len * 6823) + 2);
-	this.assertEquals(5, anim.infiniteTime);
+	this.assertEquals(4, anim.infiniteTime);
+	anim.timeShift(-4);
+	this.assertEquals(0, anim.infiniteTime);
 
+	// Floats
+	anim.timeShift(-3.2);
+	this.assertEqualFloats(3.8, anim.infiniteTime);
+	anim.timeShift(9.5);
+	this.assertEqualFloats(6.3, anim.infiniteTime);
+	anim.timeShift(-6.3);
+	this.assertEqualFloats(0, anim.infiniteTime);
+});
+
+suite.add('testFrameDig', function() {
+	var anim = new Engine.UVAnimator(new THREE.Geometry());
+	anim.addFrame('A', 4);
+	anim.addFrame('B', 2);
+	anim.addFrame('C', 3);
+	this.assertEquals('A', anim.getFrameAtTime(0).uvMap);
+	this.assertEquals('A', anim.getFrameAtTime(3).uvMap);
+	this.assertEquals('B', anim.getFrameAtTime(4).uvMap);
+	this.assertEquals('C', anim.getFrameAtTime(8.999).uvMap);
 });
 
 test.add(suite);
