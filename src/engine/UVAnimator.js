57b20e45db2bd23cd4f61548715e220f1f102f9b
Pontus Persson <pom@spotify.com>
2015-01-15 09:03:14 +0100

    Update UVAnimator to use Timeline properly.
    Change-Id: I5b8d4be795587e2b25baa497fd2239a14dbb0858

--- src/engine/UVAnimator.js
+++ src/engine/UVAnimator.js
@@ -1,70 +1,31 @@
-Engine.UVAnimator = function(geometry)
+Engine.UVAnimator = function(timeline, geometry)
 {
+    if (timeline instanceof Engine.Timeline === false) {
+        throw new Error('Invalid timeline');
+    }
+
     if (geometry instanceof THREE.Geometry === false) {
         throw new Error('Invalid geometry');
     }
 
     var self = this;
-    var timer;
-    var index = 0;
-    self.frames = [];
-    self.accumulatedTime = 0;
-    self.infiniteTime = 0;
-    self.totalDuration = 0;
-
-    self.addFrame = function(uvMap, duration)
-    {
-        self.frames.push({
-            'uvMap': uvMap,
-            'duration': duration
-        });
-        self.totalDuration += duration;
-    }
-
-    self.getFrameAtTime = function(time)
-    {
-        var i = 0, incrementalTime = 0;
-        do {
-            var frame = self.frames[i++];
-            incrementalTime += frame.duration;
-        } while (time >= incrementalTime);
-        return frame;
-    }
+    var isPlaying = false;
 
     self.pause = function()
     {
-        clearTimeout(timer);
+        isPlaying = false;
     }
 
     self.play = function()
     {
-        clearTimeout(timer);
-        self.update();
-        timer = setTimeout(self.play, self.frames[index].duration * 1000);
         isPlaying = true;
-        self.step(1);
     }
 
-    self.step = function(steps)
+    self.update = function()
     {
-        index = (index + steps) % self.frames.length;
-    }
-
-    self.stop = function()
-    {
-        self.pause();
-        index = 0;
-    }
-
-    self.timeShift = function(diff)
-    {
-        self.accumulatedTime += diff;
-        self.infiniteTime = (self.accumulatedTime % self.totalDuration + self.totalDuration) % self.totalDuration;
-    }
-
-    self.update = function(frame)
-    {
-        geometry.faceVertexUvs[0] = self.frames[index].uvMap;
+        geometry.faceVertexUvs[0] = timeline.getValue();
         geometry.uvsNeedUpdate = true;
     }
+
+    timeline.onChange(self.update);
 }
