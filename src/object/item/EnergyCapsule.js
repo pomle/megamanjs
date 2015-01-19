5b131d3442476a03b7e5171ac3df0983244bbdf0
Pontus Persson <pom@spotify.com>
2015-01-19 03:50:17 +0100

    Refactor PowerUps.
    Use sprite loader for graphics to remove superflous code.

--- src/object/item/EnergyCapsule.js
+++ src/object/item/EnergyCapsule.js
@@ -2,26 +2,14 @@ Engine.assets.objects.items.EnergyCapsule = function()
 {
     Engine.assets.objects.Item.call(this);
 
-    this.addCollisionZone(8, 0, 0);
-
-    this.capacity = 100;
-
-    var texture = THREE.ImageUtils.loadTexture('sprites/powerup/energy-capsule.gif');
-
-    var material = new THREE.MeshLambertMaterial({});
-    material.transparent = true;
-    material.map = texture;
-
-    var model = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry (16, 16),
-        material
-    );
-
-    this.sprite = new Engine.Sprite(texture);
-    this.sprite.addFrames([.1,.1]);
+    var model = Engine.Util.createSprite('powerup/energy-capsule.gif', 16, 16);
+    this.sprite = new Engine.Sprite(model.material.map);
+    this.sprite.addFrame(.1);
+    this.sprite.addFrame(.1);
     this.sprite.play();
 
     this.setModel(model);
+    this.addCollisionZone(8, 0, 0);
 }
 
 Engine.assets.objects.items.EnergyCapsule.prototype = Object.create(Engine.assets.objects.Item.prototype);
--- src/object/item/EnergyTank.js
+++ src/object/item/EnergyTank.js
@@ -2,27 +2,16 @@ Engine.assets.objects.items.EnergyTank = function()
 {
     Engine.assets.objects.Item.call(this);
 
-    this.addCollisionZone(8, 0, 0);
-
     this.capacity = 30;
 
-    var texture = THREE.ImageUtils.loadTexture('sprites/powerup/energy-tank-large.gif');
-
-    var material = new THREE.MeshLambertMaterial({});
-    material.transparent = true;
-    material.map = texture;
-
-    var model = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry (16, 16),
-        material
-    );
-
-    this.sprite = new Engine.Sprite(texture);
+    var model = Engine.Util.createSprite('powerup/energy-tank-large.gif', 16, 16);
+    this.sprite = new Engine.Sprite(model.material.map);
     this.sprite.addFrame(.1);
     this.sprite.addFrame(.1);
     this.sprite.play();
 
     this.setModel(model);
+    this.addCollisionZone(8, 0, 0);
 }
 
 Engine.assets.objects.items.EnergyTank.prototype = Object.create(Engine.assets.objects.Item.prototype);
