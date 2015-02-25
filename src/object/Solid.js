da15783f9c0de605fc403525f7b21bf0152b29bc
Pontus Persson <pom@spotify.com>
2015-02-25 22:30:51 +0100

    Alternate solid collision detection.
    Change-Id: Ia9fdbb42ec9f90e5dd1c99fd4af51ff7f875a72f

--- src/object/Solid.js
+++ src/object/Solid.js
@@ -1,10 +1,16 @@
 Engine.assets.Solid = function()
 {
     Engine.assets.Object.call(this);
+    this.buffer = new THREE.Vector2();
 }
 
 Engine.assets.Solid.prototype = Object.create(Engine.assets.Object.prototype);
 Engine.assets.Solid.constructor = Engine.assets.Solid;
+
+Engine.assets.Solid.prototype.TOP = 't';
+Engine.assets.Solid.prototype.BOTTOM = 'b';
+Engine.assets.Solid.prototype.LEFT = 'l';
+Engine.assets.Solid.prototype.RIGHT = 'r';
 
 Engine.assets.Solid.prototype.collides = function(subject, ourZone, theirZone)
 {
@@ -15,35 +21,44 @@ Engine.assets.Solid.prototype.collides = function(subject, ourZone, theirZone)
     var our = new Engine.Collision.CollisionProperty(this.model, ourZone);
     var their = new Engine.Collision.CollisionProperty(subject.model, theirZone);
 
-    if (subject.speed.y && their.r > our.l && their.l < our.r) {
-        if (subject.speed.y < 0) {
-            subject.model.position.y = our.t + (their.h / 2);
-            subject.speed.y = 0;
-            subject.isSupported = true;
-            subject.isSupportedUntil.x1 = our.l - (their.w / 2);
-            subject.isSupportedUntil.x2 = our.r + (their.w / 2);
-        }
-        else {
-            subject.model.position.y = our.b - (their.h / 2);
-            subject.jumpEnd();
-            subject.speed.y = -(subject.speed.y / 5);
+    /*this.buffer.x = subject.speed.x * subject.deltaTime;
+    this.buffer.y = subject.speed.y * subject.deltaTime;*/
+
+    var distances = {};
+        distances[this.TOP] = Math.abs(their.b - our.t);
+        distances[this.BOTTOM] = Math.abs(their.t - our.b);
+        distances[this.LEFT] = Math.abs(their.r - our.l);
+        distances[this.RIGHT] = Math.abs(their.l - our.r);
+
+    var min, n;
+    for (var d in distances) {
+        if (min === undefined || distances[d] < min) {
+            min = distances[d];
+            n = d;
         }
     }
-    else if (subject.speed.x && their.b < our.t && their.t > our.b) {
-        var inhibitor = {
-            'y1': our.t + (their.h / 2),
-            'y2': our.b - (their.h / 2),
-        };
-        if (subject.speed.x > 0) {
-            subject.moveSpeed = 0;
+
+    switch (n) {
+        case this.TOP:
+            subject.model.position.y = our.t + (their.h / 2);
+            subject.momentumSpeed.copy(this.speed);
+            //subject.isSupported = true;
+            break;
+        case this.BOTTOM:
+            subject.model.position.y = our.b - (their.h / 2);
+            subject.momentumSpeed.y = this.momentumSpeed.y - (subject.momentumSpeed.y / 5);
+            subject.jumpEnd();
+            break;
+        case this.LEFT:
             subject.model.position.x = our.l - (their.w / 2);
-            subject.movementInhibitor.r = inhibitor;
-        }
-        else {
+            //subject.momentumSpeed.x = 0;
             subject.moveSpeed = 0;
+            break;
+        case this.RIGHT:
             subject.model.position.x = our.r + (their.w / 2);
-            subject.movementInhibitor.l = inhibitor;
-        }
+            //subject.momentumSpeed.x = 0;
+            subject.moveSpeed = 0;
+            break;
     }
 }
 
