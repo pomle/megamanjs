2f45eb7f0fed9a9b47acb6eb33d53817f654130a
Pontus Persson <pom@spotify.com>
2015-03-05 14:21:59 +0100

    Correct comment.
    Change-Id: I262484070b34cea93cdc5d8f83d2536a480b6086

--- src/engine/Engine.js
+++ src/engine/Engine.js
@@ -121,7 +121,7 @@ Engine.Util = {
 
     getTexture: function(url)
     {
-        /* Default to 8x the sprite size before loading to memory. */
+        /* Default to 4x the sprite size before loading to memory. */
         return Engine.Util.getScaledTexture(url, 4);
     },
 
