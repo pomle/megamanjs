015c88f206a7a08830fa794ab229dbe964bf846a
Pontus Persson <pom@spotify.com>
2015-03-05 21:54:38 +0100

    Update explosion sprite.
    * Format PNG.
    * Add sweat.

--- src/object/decoration/Explosion.js
+++ src/object/decoration/Explosion.js
@@ -2,7 +2,7 @@ Engine.assets.decorations.Explosion = function()
 {
     Engine.assets.Object.call(this);
 
-    var model = Engine.Util.createSprite('explosions.gif', 48, 48);
+    var model = Engine.Util.createSprite('explosions.png', 48, 48);
     this.sprites = new Engine.SpriteManager(model, 48, 48 , 256, 256);
 
     var explosion = this.sprites.addSprite('explosion');
