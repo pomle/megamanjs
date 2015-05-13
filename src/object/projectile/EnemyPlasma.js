133008b22438326d595d72e565e3847391afe166
Pontus Persson <pom@spotify.com>
2015-05-13 09:36:27 +0200

    Add EnemyPlasma weapon and projectile.
    Change-Id: I726964a9904f12e4a0ed568331a3add193bd5c28

---
+++ src/object/projectile/EnemyPlasma.js
@@ -0,0 +1,17 @@
+Engine.assets.projectiles.EnemyPlasma = function()
+{
+    Engine.assets.Projectile.call(this);
+
+    var model = Engine.SpriteManager.createSingleTile('projectile/tiles.gif', 8, 8, 48, 0, 128, 128);
+
+    this.mass = 0;
+    this.setModel(model);
+    this.addCollisionZone(4, 0, 0);
+    this.setDamage(5);
+    this.setVelocity(240);
+}
+
+Engine.assets.projectiles.EnemyPlasma.prototype = Object.create(Engine.assets.Projectile.prototype);
+Engine.assets.projectiles.EnemyPlasma.constructor = Engine.assets.Projectile;
+
+
---
+++ src/object/weapon/EnemyPlasma.js
@@ -0,0 +1,19 @@
+Engine.assets.weapons.EnemyPlasma = function()
+{
+    Engine.assets.Weapon.call(this);
+}
+
+Engine.assets.weapons.EnemyPlasma.prototype = Object.create(Engine.assets.Weapon.prototype);
+Engine.assets.weapons.EnemyPlasma.constructor = Engine.assets.Weapon;
+
+Engine.assets.weapons.EnemyPlasma.prototype.fire = function()
+{
+    if (!Engine.assets.Weapon.prototype.fire.call(this)) {
+        return false;
+    }
+    var projectile = new Engine.assets.projectiles.EnemyPlasma();
+    projectile.setEmitter(this.user);
+    projectile.inertia.x = projectile.velocity * this.user.direction;
+    this.user.scene.addObject(projectile);
+    return true;
+}
