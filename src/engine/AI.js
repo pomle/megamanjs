b009801cf2241fb1c97b90261772aaa0e7f682fd
Pontus Persson <pom@spotify.com>
2015-05-13 09:24:02 +0200

    Add AI class.
    Change-Id: I30a4391657ce7d61956dd724ad7dd4ee38b5a9e8

--- src/engine/AI.js
+++ src/engine/AI.js
@@ -1,3 +1,45 @@
-Engine.AI = function()
+Engine.AI = function(object)
 {
+    this.object = object;
+    this.target = undefined;
+}
+
+Engine.AI.prototype.faceObject = function(object)
+{
+    this.object.setDirection(object.position.x > this.object.position.x
+                             ? this.object.RIGHT
+                             : this.object.LEFT);
+}
+
+Engine.AI.prototype.faceTarget = function()
+{
+    if (!this.target) {
+        return false;
+    }
+    return this.faceObject(this.target);
+}
+
+Engine.AI.prototype.findPlayer = function()
+{
+    if (this.target && this.target.isPlayer) {
+        return true;
+    }
+
+    var o;
+    for (var i in this.object.scene.objects) {
+        o = this.object.scene.objects[i];
+        if (o.isPlayer) {
+            this.setTarget(o);
+            return true;
+        }
+    }
+    return false;
+}
+
+Engine.AI.prototype.setTarget = function(object)
+{
+    if (object instanceof Engine.assets.Object !== true) {
+        throw new Error("Target must be object");
+    }
+    this.target = object;
 }
