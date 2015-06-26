755221eb28e304fb7f8aebfdfef5640117b87509
Pontus Persson <pom@spotify.com>
2015-06-26 10:28:57 +0200

    StageSelect prototype.

---
+++ src/engine/scenes/StageSelect.js
@@ -0,0 +1,154 @@
+Engine.scenes.StageSelect = function()
+{
+    Engine.Scene.call(this);
+    this.camera.camera.position.z = 120;
+    this.cameraDesiredPosition = new THREE.Vector3();
+    this.cameraSmoothing = 20;
+    this.currentIndex = undefined;
+    this.stages = [];
+    this.rowLength = 3;
+    this.spacing = {
+        x: 64,
+        y: -64,
+    };
+    this.indicatorInterval = 1/8;
+    this.indicatorStateTimer = 0;
+
+    this.background = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(500, 500),
+        new THREE.MeshBasicMaterial({
+            color: 'blue'
+        }));
+    this.scene.add(this.background);
+}
+
+Engine.scenes.StageSelect.loadFromXml = function(url, callback)
+{
+    Engine.Util.asyncLoadXml(url, function(xml, baseUrl) {
+        var sceneXml = xml.children('stage-select');
+        var scene = new Engine.scenes.StageSelect();
+
+        var spriteUrl = sceneXml.attr('url');
+        var spriteW = parseFloat(sceneXml.attr('w'));
+        var spriteH = parseFloat(sceneXml.attr('h'));
+
+        var backgroundXml = sceneXml.children('background');
+        scene.setBackgroundColor(backgroundXml.attr('color'));
+
+        var indicatorXml = sceneXml.children('indicator');
+        scene.setIndicator(Engine.SpriteManager.createSingleTile(
+            spriteUrl,
+            parseFloat(indicatorXml.attr('w')), parseFloat(indicatorXml.attr('h')),
+            parseFloat(indicatorXml.attr('x')), parseFloat(indicatorXml.attr('y')),
+            spriteW, spriteH));
+
+        var frameXml = sceneXml.children('frame');
+        scene.setFrame(Engine.SpriteManager.createSingleTile(
+            spriteUrl,
+            parseFloat(frameXml.attr('w')), parseFloat(frameXml.attr('h')),
+            parseFloat(frameXml.attr('x')), parseFloat(frameXml.attr('y')),
+            spriteW, spriteH));
+
+        sceneXml.find('> stage').each(function() {
+            var stageXml = $(this);
+            var avatar = Engine.SpriteManager.createSingleTile(
+                spriteUrl,
+                parseFloat(stageXml.attr('w')), parseFloat(stageXml.attr('h')),
+                parseFloat(stageXml.attr('x')), parseFloat(stageXml.attr('y')),
+                spriteW, spriteH);
+            var index = parseFloat(stageXml.attr('index'));
+            var name = stageXml.attr('name');
+            var src = stageXml.attr('src');
+            scene.addStage(avatar, name, src);
+        });
+
+        scene.equalize();
+        callback(scene);
+    });
+}
+
+Engine.scenes.StageSelect.prototype = Object.create(Engine.Scene.prototype);
+Engine.scenes.StageSelect.prototype.constructor = Engine.scenes.StageSelect;
+
+Engine.scenes.StageSelect.prototype.addStage = function(avatar, name, src)
+{
+    var x = this.stages.length % this.rowLength;
+    var y = Math.floor(this.stages.length / this.rowLength);
+
+    var pos = new THREE.Vector2(this.spacing.x * x, this.spacing.y * y);
+    var frame = this.frame.clone();
+    this.stages.push({
+        "avatar": avatar,
+        "name": name,
+        "src": src,
+    });
+    frame.position.set(pos.x, pos.y, 0);
+    avatar.position.set(pos.x, pos.y, .1);
+    this.scene.add(frame);
+    this.scene.add(avatar);
+}
+
+Engine.scenes.StageSelect.prototype.equalize = function()
+{
+    var centerIndex = 4;
+    var center = this.stages[centerIndex].avatar.position;
+    this.camera.camera.position.copy(center);
+    this.camera.camera.position.z = 100;
+    this.cameraDesiredPosition.copy(center);
+    this.cameraDesiredPosition.z = 140;
+
+    this.selectIndex(centerIndex);
+    this.background.position.copy(center);
+    this.background.position.z -= 10;
+}
+
+Engine.scenes.StageSelect.prototype.setBackgroundColor = function(hexcolor)
+{
+    this.background.material.color.setHex(hexcolor);
+}
+
+Engine.scenes.StageSelect.prototype.setFrame = function(model)
+{
+    this.frame = model;
+}
+
+Engine.scenes.StageSelect.prototype.steer = function(x, y)
+{
+    var desiredIndex = this.currentIndex + x + y * this.rowLength;
+    var len = this.stages.length;
+    var newIndex = (desiredIndex % len + len) % len;
+    this.selectIndex(newIndex);
+}
+
+Engine.scenes.StageSelect.prototype.selectIndex = function(index)
+{
+    var avatar = this.stages[index].avatar;
+    this.indicator.position.x = avatar.position.x;
+    this.indicator.position.y = avatar.position.y;
+    //this.cameraDesiredPosition.copy(this.indicator.position);
+    //this.cameraDesiredPosition.z = 140;
+    this.currentIndex = index;
+}
+
+Engine.scenes.StageSelect.prototype.setIndicator = function(model)
+{
+    this.indicator = model;
+    this.indicator.position.z = .1;
+    this.scene.add(model);
+}
+
+Engine.scenes.StageSelect.prototype.updateTime = function(dt)
+{
+    this.indicatorStateTimer += dt;
+    if (this.indicatorStateTimer >= this.indicatorInterval) {
+        this.indicator.visible = !this.indicator.visible;
+        this.indicatorStateTimer = 0;
+    }
+
+    if (this.camera.camera.position.distanceToSquared(this.cameraDesiredPosition) > 1) {
+        var intermediate = this.cameraDesiredPosition.clone().sub(this.camera.camera.position).divideScalar(this.cameraSmoothing);
+        this.camera.camera.position.add(intermediate);
+    }
+
+    Engine.Scene.prototype.updateTime.call(this, dt);
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(800, 532);
-document.getElementById('screen').appendChild(renderer.domElement);
-
-var Megaman2 = function()
+var Megaman = function()
 {
     this.engine = undefined;
     this.player = undefined;
-    this.level = new Megaman2.LevelRunner(this);
+    this.level = undefined;
 
     window.addEventListener('focus', function() {
         if (this.engine && !this.engine.isRunning) {
@@ -20,38 +16,126 @@ var Megaman2 = function()
     }.bind(this));
 }
 
-Megaman2.prototype.loadLevel = function(xmlUrl)
+Megaman.createFromXML = function(gameXml, baseUrl)
 {
-<<<<<<< Updated upstream
-	window.clearTimeout(this.levelLoadTimeout);
-	this.engine.pause();
-	this.engine.scene = undefined;
-	Engine.scenes.Level.Util.loadFromXML(xmlUrl, function(level) {
-		level.addObject(this.player.character, level.checkPoints[0].pos.x, level.checkPoints[0].pos.y);
-		level.camera.follow(this.player.character);
-		this.engine.scene = level;
-		this.levelLoadTimeout = window.setTimeout(this.engine.run.bind(this.engine), 200);
-	}.bind(this));
-=======
-    window.clearTimeout(this.levelLoadTimeout);
+    var baseUrl = baseUrl || '';
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(800, 532);
+    document.getElementById('screen').appendChild(renderer.domElement);
+
+    var game = new Megaman();
+    game.engine = new Engine(renderer);
+
+    game.player = new Megaman.Player();
+    game.player.hud = new Hud($('#screen'));
+
+    gameXml.find('> weapons > weapon').each(function() {
+        var weaponXml = $(this);
+        var code = weaponXml.attr('code');
+        var name = weaponXml.attr('name');
+
+        if (!Engine.assets.weapons[name]) {
+            throw new Error('Weapon ' + name + ' does not exist');
+        }
+        var weapon = new Engine.assets.weapons[name]();
+        weapon.code = code;
+        game.player.weapons[code] = weapon;
+    });
+
+    game.player.setCharacter(new Engine.assets.objects.characters.Megaman());
+    game.player.hud.equipCharacter(game.player.character);
+    game.player.character.invincibilityDuration = 2;
+    game.player.input = new Engine.Keyboard();
+    game.player.equipWeapon('p');
+
+    game.player.input.intermittent(65,
+        function() {
+            game.player.character.moveLeftStart();
+        },
+        function() {
+            game.player.character.moveLeftEnd();
+        });
+    game.player.input.intermittent(68,
+        function() {
+            game.player.character.moveRightStart();
+        },
+        function() {
+            game.player.character.moveRightEnd();
+        });
+
+    game.player.input.intermittent(80,
+        function() {
+            game.player.character.jumpStart();
+        },
+        function() {
+            game.player.character.jumpEnd();
+        });
+    game.player.input.hit(79,
+        function() {
+            game.player.character.fire();
+        });
+    game.player.input.hit(89,
+        function() {
+            game.engine.isSimulating = !game.engine.isSimulating;
+        });
+
+    game.player.input.hit(33, function() {
+        equipWeapon(weaponIndex[++weaponIndex.selected]);
+    });
+    game.player.input.hit(34, function() {
+        equipWeapon(weaponIndex[--weaponIndex.selected]);
+    });
+
+    return game;
+}
+Megaman.loadFromXML = function(url, callback)
+{
+    Engine.Util.asyncLoadXml(url, function(xml, baseUrl) {
+        var gameXml = xml.children('game');
+        var game = Megaman.createFromXML(gameXml, baseUrl);
+        gameXml.find('> entrypoint > scene').each(function() {
+            var sceneXml = $(this);
+            var name = sceneXml.attr('name');
+            var src = baseUrl + '/' + sceneXml.attr('src');
+            if (!Engine.scenes[name]) {
+                throw new Error("Scene " + name + " does not exist");
+            }
+            Engine.scenes[name].loadFromXml(src, function(scene) {
+                game.engine.scene = scene;
+                callback(game);
+            });
+        });
+    });
+}
+
+Megaman.prototype.endLevel = function()
+{
+    this.engine.pause();
+    this.engine.scene = undefined;
+
+
+}
+
+Megaman.prototype.loadLevel = function(xmlUrl)
+{
     this.engine.pause();
     this.engine.scene = undefined;
     Engine.scenes.Level.Util.loadFromXML(xmlUrl, function(level) {
-        this.level.setLevel(level);
+        this.level = new Megaman.LevelRunner(this, level);
+        this.level.startGamePlay();
     }.bind(this));
->>>>>>> Stashed changes
 }
 
-Megaman2.Player = function()
+Megaman.Player = function()
 {
     this.character = undefined;
     this.hud = undefined;
     this.input = undefined;
-    this.lifes = 0;
+    this.lifes = 3;
     this.weapons = {};
 }
 
-Megaman2.Player.prototype.equipWeapon = function(code)
+Megaman.Player.prototype.equipWeapon = function(code)
 {
     var weapon = this.weapons[code];
     weapon.code = code;
@@ -59,98 +143,72 @@ Megaman2.Player.prototype.equipWeapon = function(code)
     this.hud.equipWeapon(weapon);
 }
 
-Megaman2.Player.prototype.setCharacter = function(character)
+Megaman.Player.prototype.setCharacter = function(character)
 {
     this.character = character;
-    this.character.isPlayer = true;
 }
 
-Megaman2.LevelRunner = function(game)
+Megaman.LevelRunner = function(game, level)
 {
-    this.game = game;
-    this.level = undefined;
-}
-
-Megaman2.LevelRunner.prototype.setLevel = function(level)
-{
-    if (solid instanceof Engine.scenes.Level === false) {
+    if (game instanceof Megaman === false) {
+        throw new Error('Invalid game');
+    }
+    if (level instanceof Engine.scenes.Level === false) {
         throw new Error('Invalid level');
     }
 
-    this.unsetLevel();
-    this.game.engine.scene = level;
+    this.checkPointIndex = 0;
+    this.game = game;
+    this.level = level;
+    this.game.engine.scene = this.level;
+    this.resetPlayer();
+
+    this.deathCountdown = 0;
+    this.deathRespawnTime = 4;
+
+    this.game.engine.events.simulate.push(this.simulateListener.bind(this));
+}
+
+Megaman.LevelRunner.prototype.simulateListener = function()
+{
+    if (this.deathCountdown === 0 && this.game.player.character.health.depleted()) {
+        this.game.player.lifes--;
+        this.deathCountdown = this.game.engine.timeElapsedTotal + this.deathRespawnTime;
+    }
+    if (this.deathCountdown > 0 && this.game.engine.timeElapsedTotal > this.deathCountdown) {
+        if (this.game.player.lifes == 0) {
+            this.game.endLevel();
+        }
+        else {
+            this.resetPlayer();
+        }
+    }
+}
+
+Megaman.LevelRunner.prototype.resetPlayer = function()
+{
+    this.deathCountdown = 0;
+    var player = this.game.player.character;
+    player.isPlayer = true;
+    player.health.set(player.health.max);
+    this.level.removeObject(player);
+    this.level.addObject(player,
+                    this.level.checkPoints[this.checkPointIndex].pos.x,
+                    this.level.checkPoints[this.checkPointIndex].pos.y);
+    this.level.camera.follow(this.game.player.character);
+}
+
+Megaman.LevelRunner.prototype.startGamePlay = function()
+{
     this.game.engine.run();
 }
 
-Megaman2.LevelRunner.prototype.unsetLevel = function()
-{
-    if (this.level === undefined) {
-        return;
-    }
 
-    this.game.engine.pause();
-    this.level = undefined;
-}
-
-
-
-
-var game = new Megaman2();
-game.engine = new Engine(renderer);
-
-game.player = new Megaman2.Player();
-game.player.hud = new Hud($('#screen'));
-game.player.weapons = {
-    'p': new Engine.assets.weapons.Plasma(),
-    'a': new Engine.assets.weapons.AirShooter(),
-    'm': new Engine.assets.weapons.MetalBlade(),
-    'c': new Engine.assets.weapons.CrashBomber()
-};
-game.player.setCharacter(new Engine.assets.objects.characters.Megaman());
-game.player.hud.equipCharacter(game.player.character);
-game.player.character.invincibilityDuration = 2;
-game.player.input = new Engine.Keyboard();
-game.player.equipWeapon('p');
-
-game.player.input.intermittent(65,
-    function() {
-        game.player.character.moveLeftStart();
-    },
-    function() {
-        game.player.character.moveLeftEnd();
-    });
-game.player.input.intermittent(68,
-    function() {
-        game.player.character.moveRightStart();
-    },
-    function() {
-        game.player.character.moveRightEnd();
-    });
-
-game.player.input.intermittent(80,
-    function() {
-        game.player.character.jumpStart();
-    },
-    function() {
-        game.player.character.jumpEnd();
-    });
-game.player.input.hit(79,
-    function() {
-        game.player.character.fire();
-    });
-game.player.input.hit(89,
-    function() {
-        game.engine.isSimulating = !game.engine.isSimulating;
-    });
-
-game.player.input.hit(33, function() {
-    equipWeapon(weaponIndex[++weaponIndex.selected]);
+var game;
+Megaman.loadFromXML('resource/Megaman2.xml', function(g) {
+    game = g;
+    game.engine.run();
 });
-game.player.input.hit(34, function() {
-    equipWeapon(weaponIndex[--weaponIndex.selected]);
-});
-
-game.loadLevel('levels/flashman/Flashman.xml');
 
 
 var pendelum = function(dt)
