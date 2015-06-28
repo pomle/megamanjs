a92a3e7c4211738865551dfe0745530cc65e3ddc
Pontus Persson <pom@spotify.com>
2015-06-28 23:47:45 +0200

    Refactor Camera.

--- src/engine/Camera.js
+++ src/engine/Camera.js
@@ -1,27 +1,36 @@
 Engine.Camera = function(camera)
 {
     this.camera = camera;
+    this.desiredPosition = undefined;
     this.followObject = undefined;
     this.followOffset = new THREE.Vector2(0, 25);
     this.followLookAhead = new THREE.Vector2(.5, .2);
+    this.smoothing = 20;
+
 }
 
-Engine.Camera.prototype.follow = function(object, distance)
+Engine.Camera.prototype.follow = function(object, offset)
 {
     this.followObject = object;
+    if (offset) {
+        this.followOffset = offset;
+    } else {
+        this.followOffset = new THREE.Vector2(0, 0);
+    }
+
 }
 
 Engine.Camera.prototype.jumpTo = function(pos)
 {
     this.unfollow();
+    this.desiredPosition = undefined;
     this.camera.position.x = pos.x;
     this.camera.position.y = pos.y;
 }
 
-Engine.Camera.prototype.panTo = function(x, y)
+Engine.Camera.prototype.panTo = function(pos)
 {
-    this.camera.position.x = x;
-    this.camera.position.y = y;
+    this.desiredPosition = pos.clone();
 }
 
 Engine.Camera.prototype.unfollow = function()
@@ -31,8 +40,13 @@ Engine.Camera.prototype.unfollow = function()
 
 Engine.Camera.prototype.updateTime = function(timeElapsed)
 {
+    if (this.desiredPosition) {
+        var move = this.desiredPosition.clone().sub(this.camera.position).divideScalar(this.smoothing);
+        this.camera.position.x += move.x;
+        this.camera.position.y += move.y;
+    }
+
     if (this.followObject) {
-        this.panTo(this.followObject.model.position.x,
-                   this.followObject.model.position.y);
+        this.desiredPosition = this.followObject.position.clone().add(this.followOffset);
     }
 }
