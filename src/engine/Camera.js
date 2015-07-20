f0ced4108537e62638fab6c1307c8bbc20ebff61
Pontus Persson <pom@spotify.com>
2015-07-21 00:19:01 +0200

    Camera "howMuchAmIInside" algorithm.

--- src/engine/Camera.js
+++ src/engine/Camera.js
@@ -11,11 +11,11 @@ Engine.Camera = function(camera)
     this.velocity = new THREE.Vector2(0, 0);
 }
 
-Engine.Camera.prototype.addPath = function(x1, x2, y1, y2)
+Engine.Camera.prototype.addPath = function(x1, y1, x2, y2)
 {
     this.paths.push([
-        new THREE.Vector2(x1, x2),
-        new THREE.Vector2(y1, y2),
+        new THREE.Vector2(x1, y1),
+        new THREE.Vector2(x2, y2),
     ]);
 }
 
@@ -61,27 +61,32 @@ Engine.Camera.prototype.updateTime = function(timeElapsed)
 
     if (this.desiredPosition) {
         if (this.obeyPaths && this.paths.length) {
-            var distances = [];
+            var distances = {x:[], y:[]};
             var x = this.desiredPosition.x;
             var y = this.desiredPosition.y;
             for (var i in this.paths) {
                 var path = this.paths[i];
-                distances[i] = Math.min(
-                    Math.abs(x - path[0].x),
-                    Math.abs(x - path[1].x),
-                    Math.abs(y - path[0].y),
-                    Math.abs(y - path[1].y));
+                distances.x[i] = howMuchInsideAmI(x, path[0].x, path[1].x);
+                distances.y[i] = howMuchInsideAmI(y, path[0].y, path[1].y);
+                distances.x[i] += distances.y[i];
+                distances.y[i] = distances.x[i];
             }
-            var minIndex = 0, min = distances[minIndex];
-            for (var i = 1, l = distances.length; i < l; i++) {
-                if (distances[i] < min) {
-                    minIndex = i;
-                    min = distances[i];
+            var xMinIndex = 0,
+                xMin = distances.x[xMinIndex],
+                yMinIndex = 0,
+                yMin = distances.y[yMinIndex];
+            for (var l = i, i = 1; i <= l; i++) {
+                if (distances.x[i] < xMin) {
+                    xMinIndex = i;
+                    xMin = distances.x[i];
+                }
+                if (distances.y[i] < yMin) {
+                    yMinIndex = i;
+                    yMin = distances.x[i];
                 }
             }
-            var path = this.paths[minIndex];
-            this.desiredPosition.x = Engine.Math.clamp(this.desiredPosition.x, path[0].x, path[1].x);
-            this.desiredPosition.y = Engine.Math.clamp(this.desiredPosition.y, path[0].y, path[1].y);
+            this.desiredPosition.x = Engine.Math.clamp(this.desiredPosition.x, this.paths[xMinIndex][0].x, this.paths[xMinIndex][1].x);
+            this.desiredPosition.y = Engine.Math.clamp(this.desiredPosition.y, this.paths[yMinIndex][1].y, this.paths[yMinIndex][0].y);
         }
 
         this.velocity = this.desiredPosition.clone().sub(this.camera.position);
@@ -92,4 +97,18 @@ Engine.Camera.prototype.updateTime = function(timeElapsed)
 
     this.camera.position.x += this.velocity.x;
     this.camera.position.y += this.velocity.y;
+}
+
+function howMuchInsideAmI(x, x1, x2) {
+    if (x1 > x2) {
+        x1 = x1 + x2;
+        x2 = x1 - x2;
+        x1 = x1 - x2;
+    }
+    var val = Math.abs(x1 - x) + Math.abs(x - x2);
+    val -= Math.abs(x2 - x1);
+    if (val == 0) {
+        val -= Math.min(x2 - x, x - x1);
+    }
+    return val;
 }
