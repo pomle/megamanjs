6f3399805c22b5fac6a72c7e1509e8564970d8d1
Pontus Persson <pom@spotify.com>
2015-07-21 14:48:53 +0200

    Decouple Math libs.

--- src/engine/Engine.js
+++ src/engine/Engine.js
@@ -72,43 +72,6 @@ Engine.prototype.run = function()
     this.loop();
 }
 
-Engine.Math = {
-    applyRatio: function(ratio, h, l)
-    {
-        return (h - l) * ratio + l;
-    },
-
-    clamp: function(v, min, max)
-    {
-        return Math.min(max, Math.max(min, v));
-    },
-
-    close: function(x, x1, x2) {
-        if (x1 > x2) {
-            x1 += x2;
-            x2 = x1 - x2;
-            x1 = x1 - x2;
-        }
-        var val = Math.abs(x1 - x) + Math.abs(x - x2);
-        val -= Math.abs(x2 - x1);
-        if (val == 0) {
-            val -= Math.min(x2 - x, x - x1);
-        }
-        return val;
-    },
-
-    findRatio: function(pos, h, l)
-    {
-        return (pos - l) / (h - l);
-    },
-
-    squaredDistance: function(v1, v2)
-    {
-        var dx = v1.x - v2.x,
-            dy = v1.y - v2.y;
-        return dx * dx + dy * dy;
-    },
-}
 
 Engine.TextureManager = {
     'scale': 4,
