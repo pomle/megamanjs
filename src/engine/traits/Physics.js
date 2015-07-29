f33b1f0c3b11ee4026d45bfe6107db8e572720f9
Pontus Persson <pom@spotify.com>
2015-07-29 07:36:46 +0200

    Move gravity from Level to Physics trait.

--- src/engine/traits/Physics.js
+++ src/engine/traits/Physics.js
@@ -16,10 +16,28 @@ Engine.traits.Physics.prototype.NAME = 'physics';
 Engine.traits.Physics.prototype.__timeshift = function(dt)
 {
     if (this.enabled && dt) {
+        /* It is very important that gravity is applied before the
+           velocity is summed. Otherwise objects will not be pulled into the
+           ground between ticks and collision detection in resting
+           state will only happen every other tick. */
+        this._applyGravity(dt);
         this.object.velocity.set(0, 0);
         this.object.velocity.add(this.inertia);
         this.object.velocity.add(this.momentum);
     }
+}
+
+Engine.traits.Physics.prototype._applyGravity = function(dt)
+{
+    if (this.mass == 0) {
+        return false;
+    }
+    var g = this.object.scene.gravityForce;
+    if (g && (g.x || g.y)) {
+        this.inertia.x += g.x * dt;
+        this.inertia.y -= g.y * dt;
+    }
+    return true;
 }
 
 Engine.traits.Physics.prototype.bump = function(x, y)
