accd27102bf4f34af718e25cb6ca5f8cd65266b9
Pontus Persson <pom@spotify.com>
2015-07-30 14:18:09 +0200

    Add extend and mixin util functions to Util.

--- src/engine/Util.js
+++ src/engine/Util.js
@@ -14,6 +14,25 @@ Engine.Util = {
         xmlhttp.send();
     },
 
+    extend: function(child, parent)
+    {
+        child.prototype = Object.create(parent.prototype);
+        child.constructor = parent;
+    },
+
+    mixin: function()
+    {
+        var subject = arguments[0];
+        for (var i = 1, l = arguments.length; i < l; ++i) {
+            var source = arguments[i];
+            for (var prop in source.prototype) {
+                if (source.prototype.hasOwnProperty(prop)) {
+                    subject.prototype[prop] = source.prototype[prop];
+                }
+            }
+        }
+    },
+
     string: {
         fill: function(x, n)
         {
