49a025d483e004917250e470574aec55551e41e1
Pontus Persson <pom@spotify.com>
2015-08-04 20:26:57 +0200

    Refactor ResourceManager.

--- src/game/ResourceManager.js
+++ src/game/ResourceManager.js
@@ -2,33 +2,55 @@ Game.ResourceManager = function()
 {
     this.textureScale = 4;
 
+    /* These must be defined in order of specificity. */
+    this.typeMap = {
+        'character': Game.objects.Character,
+        'weapon': Game.objects.Weapon,
+        'object': Engine.Object,
+        'texture': THREE.Texture,
+    }
+
     this.items = {};
 }
 
-Game.ResourceManager.prototype._addResource = function(type, name, object)
+Game.ResourceManager.prototype._addResource = function(type, id, object)
 {
+    if (!type) {
+        throw new Error('Empty type');
+    }
+    if (!id) {
+        throw new Error('Empty id');
+    }
     if (!this.items[type]) {
         this.items[type] = {};
     }
-    if (!name) {
-        throw new Error('Empty name');
-    }
-    if (this.items[type][name]) {
-        throw new Error("Object " + name + " already defined");
+    if (this.items[type][id]) {
+        throw new Error("Object " + id + " already defined");
     }
 
-    this.items[type][name] = object;
+    this.items[type][id] = object;
 }
 
-
-Game.ResourceManager.prototype.addCharacter = function(name, object)
+Game.ResourceManager.prototype.addAuto = function(id, object)
 {
-    return this._addResource('character', name, object);
+    for (var type in this.typeMap) {
+        var proto = this.typeMap[type].prototype;
+        if (proto.isPrototypeOf(object.prototype)) {
+            this._addResource(type, id, object);
+            return true;
+        }
+    }
+    throw new Error('Could not determine type from ' + object);
 }
 
-Game.ResourceManager.prototype.addWeapon = function(name, object)
+Game.ResourceManager.prototype.addCharacter = function(id, object)
 {
-    return this._addResource('weapon', name, object);
+    return this._addResource('character', id, object);
+}
+
+Game.ResourceManager.prototype.addWeapon = function(id, object)
+{
+    return this._addResource('weapon', id, object);
 }
 
 Game.ResourceManager.prototype.createTexture = function(location, callback)
@@ -42,4 +64,12 @@ Game.ResourceManager.prototype.createTexture = function(location, callback)
         transparent: true,
     });
     return texture;
+}
+
+Game.ResourceManager.prototype.get = function(type, id)
+{
+    if (this.items[type] && this.items[type][id]) {
+        return this.items[type][id];
+    }
+    throw new Error('Could not find resource "' + id + '" of type "' + type + '"');
 }
