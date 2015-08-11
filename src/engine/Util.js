d60b9f7485951d71188f4d0042bb532d4dd4f779
Pontus Persson <pom@spotify.com>
2015-08-11 10:02:38 +0200

    Add renameFunction to Engine.Util.

--- src/engine/Util.js
+++ src/engine/Util.js
@@ -1,4 +1,9 @@
 Engine.Util = {
+    renameFunction: function (name, fn) {
+        return (new Function("return function (call) { return function " + name +
+            " () { return call(this, arguments) }; };")())(Function.apply.bind(fn));
+    },
+
     extend: function(child, parent)
     {
         child.prototype = Object.create(parent.prototype);
