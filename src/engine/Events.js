588f22c234b63fa5e2ea7278f60b3fd2988eb5ab
Pontus Persson <pom@spotify.com>
2015-08-13 22:42:00 +0200

    Make sure events class can handle in-loop unbinds.

--- src/engine/Events.js
+++ src/engine/Events.js
@@ -12,7 +12,23 @@ Engine.Events.prototype.bind = function(name, callback)
     if (!this.events[name]) {
         this.events[name] = [];
     }
+
     this.events[name].push(callback);
+    this.gc(name);
+}
+
+Engine.Events.prototype.gc = function(name)
+{
+    if (this.events[name]) {
+        var events = this.events[name];
+        for (var i = 0, l = events.length; i < l; ++i) {
+            if (events[i] === undefined) {
+                events.splice(i, 1);
+                --i;
+                --l;
+            }
+        }
+    }
 }
 
 Engine.Events.prototype.bound = function(name, callback)
@@ -28,7 +44,9 @@ Engine.Events.prototype.trigger = function(name, values)
            get the arguments to be passed as an
            array as second argument. */
         for (var i = 0, l = events.length; i < l; ++i) {
-            events[i].apply(this, values);
+            if (events[i] !== undefined) {
+                events[i].apply(this, values);
+            }
         }
         return true;
     }
@@ -41,7 +59,7 @@ Engine.Events.prototype.unbind = function(name, callback)
         var events = this.events[name];
         var i = events.indexOf(callback);
         if (i !== -1) {
-            events.splice(i, 1);
+            events[i] = undefined;
             return true;
         }
     }
