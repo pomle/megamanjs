b9bf0676264f4dfda7f8bbf68407780eec6cd88e
Pontus Persson <pom@spotify.com>
2015-08-26 22:28:06 +0200

    Upgrade Airman to XML.

--- src/game/object/character/Airman.js
+++ src/game/object/character/Airman.js
@@ -1,61 +1,27 @@
 Game.objects.characters.Airman = function()
 {
     Game.objects.Character.call(this);
-    this.contactDamage.points = 4;
-    this.jump.force = 260;
-
-    var model = Engine.SpriteManager.createSprite('bosses/airman.png', 48, 48);
-    this.sprites = new Engine.SpriteManager(model, 48, 48 , 256, 256);
-
-    var idle = this.sprites.addSprite('idle');
-    idle.addFrame(0, 0);
-
-    var fire = this.sprites.addSprite('fire');
-    fire.addFrame(96, 0);
-
-    var jump = this.sprites.addSprite('jump');
-    jump.addFrame(48, 0);
-
-    var blow = this.sprites.addSprite('blow');
-    blow.addFrame(144,  0, .12);
-    blow.addFrame(192,  0, .12);
-
-    this.setModel(model);
-
-    this.addCollisionRect(20, 28, 0, 0);
-    this.weapon.projectileEmitOffset.set(22, -1);
-
     this.isBlowing = false;
-
 }
 
-Game.objects.characters.Airman.prototype = Object.create(Game.objects.Character.prototype);
-Game.objects.characters.Airman.constructor = Game.objects.characters.Airman;
+Engine.Util.extend(Game.objects.characters.Airman,
+                   Game.objects.Character);
 
-Game.objects.characters.Airman.prototype.updateSprite = function()
+Game.objects.characters.Airman.prototype.routeAnimation = function()
 {
-    if (this.walk != 0) {
-        this.sprites.setDirection(this.direction.x);
-    }
+    var anim = this.animators[0];
 
-    if (this.isFiring) {
-        return this.sprites.selectSprite('fire');
+    if (this.weapon._firing) {
+        return anim.pickAnimation('fire');
     }
 
     if (!this.isSupported) {
-        return this.sprites.selectSprite('jump');
+        return anim.pickAnimation('jump');
     }
 
     if (this.isBlowing) {
-        return this.sprites.selectSprite('blow');
+        return anim.pickAnimation('blow');
     }
 
-    return this.sprites.selectSprite('idle');
-}
-
-Game.objects.characters.Airman.prototype.timeShift = function(dt)
-{
-    this.updateSprite();
-    this.sprites.timeShift(dt);
-    Game.objects.Character.prototype.timeShift.call(this, dt);
+    return anim.pickAnimation('idle');
 }
