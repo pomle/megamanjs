2d5c20b34ed614664bdeba5a478aa6d74451e2a5
Pontus Persson <pom@spotify.com>
2015-08-26 21:16:39 +0200

    Upgrade Crashman to XML based.

--- src/game/object/character/Crashman.js
+++ src/game/object/character/Crashman.js
@@ -1,62 +1,25 @@
 Game.objects.characters.Crashman = function()
 {
     Game.objects.Character.call(this);
-    this.contactDamage.points = 4;
-
-    var model = Engine.SpriteManager.createSprite('bosses/crashman.png', 48, 48);
-    this.sprites = new Engine.SpriteManager(model, 48, 48 , 256, 256);
-
-    var taunt = this.sprites.addSprite('taunt');
-    taunt.addFrame(0, 0);
-
-    var idle = this.sprites.addSprite('idle');
-    idle.addFrame(0, 0);
-
-    var jump = this.sprites.addSprite('jump');
-    jump.addFrame(0, 96);
-
-    var jumpFire = this.sprites.addSprite('jump-fire');
-    jumpFire.addFrame(48, 96, .05);
-    jumpFire.addFrame(96, 96, 5);
-
-    var run = this.sprites.addSprite('run');
-    run.addFrame(0,  48, .12);
-    run.addFrame(48, 48, .12);
-    run.addFrame(0,  48, .12);
-    run.addFrame(96, 48, .12);
-
-    this.jump.force = 250;
-
-    this.setModel(model);
-    this.addCollisionRect(12, 24, 0, 0);
 }
 
-Game.objects.characters.Crashman.prototype = Object.create(Game.objects.Character.prototype);
-Game.objects.characters.Crashman.constructor = Game.objects.characters.Crashman;
+Engine.Util.extend(Game.objects.characters.Crashman,
+                   Game.objects.Character);
 
-Game.objects.characters.Crashman.prototype.updateSprite = function()
+Game.objects.characters.Crashman.prototype.routeAnimation = function()
 {
-    if (this.walk != 0) {
-        this.sprites.setDirection(this.direction.x);
-    }
+    var anim = this.animators[0];
 
     if (!this.isSupported) {
-        if (this.isFiring) {
-            return this.sprites.selectSprite('jump-fire');
+        if (this.weapon._firing) {
+            return anim.pickAnimation('jump-fire');
         }
-        return this.sprites.selectSprite('jump');
+        return anim.pickAnimation('jump');
     }
 
-    if (this.moveSpeed) {
-        return this.sprites.selectSprite('run');
+    if (this.move._walkSpeed) {
+        return anim.pickAnimation('run');
     }
 
-    return this.sprites.selectSprite('idle');
-}
-
-Game.objects.characters.Crashman.prototype.timeShift = function(dt)
-{
-    this.updateSprite();
-    this.sprites.timeShift(dt);
-    Game.objects.Character.prototype.timeShift.call(this, dt);
+    return anim.pickAnimation('idle');
 }
