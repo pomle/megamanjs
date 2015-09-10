f4705faf149eda6a1a9510651f8432fd394e38b8
Pontus Persson <pom@spotify.com>
2015-09-10 17:55:35 +0200

    Decouple item creation.

--- src/tool/layout-editor.js
+++ src/tool/layout-editor.js
@@ -2,6 +2,40 @@
 var editor = {};
 
 $(function() {
+    function createPlane(size) {
+        let uniqueId = 'object_' + THREE.Math.generateUUID().replace(/-/g, '');
+
+        let objectNode = $('<object/>', editor.node).attr({
+            'id': uniqueId,
+        });
+        let geometryNode = $('<geometry/>', editor.node).attr({
+            'type': 'plane',
+            'w': size.x,
+            'h': size.y,
+            'w-segments': size.sx || 1,
+            'h-segments': size.sy || 1,
+        });
+        objectNode.append(geometryNode);
+        editor.node.object.append(objectNode);
+
+        let game = editor.game,
+            loader = new Game.Loader.XML(game),
+            parser = new Game.Loader.XML.Parser.ObjectParser(loader);
+
+        let objectRef = parser.getObject(objectNode);
+
+        let objectInstanceNode = $('<object/>', editor.node).attr({
+            'id': uniqueId,
+        });
+
+        editor.node.layout.objects.append(objectInstanceNode);
+
+        let item = new editor.item(new objectRef(), objectInstanceNode, objectRef);
+        editor.items.insert(item);
+
+        return item;
+    }
+
     function mouseSelectItem(event, viewport, items) {
         console.log(event, viewport, items);
         var vector = new THREE.Vector3(0,0,0),
@@ -531,35 +565,7 @@ $(function() {
                     size['sx'] = Math.ceil(size.x / m);
                     size['sy'] = Math.ceil(size.y / m);
 
-                    let uniqueId = 'object_' + THREE.Math.generateUUID().replace(/-/g, '');
-
-                    let objectNode = $('<object/>', editor.node).attr({
-                        'id': uniqueId,
-                    });
-                    let geometryNode = $('<geometry/>', editor.node).attr({
-                        'type': 'plane',
-                        'w': size.x,
-                        'h': size.y,
-                        'w-segments': size.sx,
-                        'h-segments': size.sy,
-                    });
-                    objectNode.append(geometryNode);
-                    editor.node.object.append(objectNode);
-
-                    let game = editor.game,
-                        loader = new Game.Loader.XML(game),
-                        parser = new Game.Loader.XML.Parser.ObjectParser(loader);
-
-                    let objectRef = parser.getObject(objectNode);
-
-                    let objectInstanceNode = $('<object/>', editor.node).attr({
-                        'id': uniqueId,
-                    });
-
-                    editor.node.layout.objects.append(objectInstanceNode);
-
-                    let item = new editor.item(new objectRef(), objectInstanceNode, objectRef);
-                    editor.items.insert(item);
+                    let item = createPlane(size);
                     item.object.moveTo(editor.marker.position);
                     item.update();
                     break;
