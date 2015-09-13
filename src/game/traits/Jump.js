7be7b174047694be148500e63f25cdc191bc0d37
Pontus Persson <pom@spotify.com>
2015-09-13 09:29:38 +0200

    Add events to Jump trait.

--- src/game/traits/Jump.js
+++ src/game/traits/Jump.js
@@ -13,6 +13,9 @@ Game.traits.Jump = function()
 Engine.Util.extend(Game.traits.Jump, Engine.Trait);
 
 Game.traits.Jump.prototype.NAME = 'jump';
+Game.traits.Jump.prototype.EVENT_JUMP_ENGAGE = 'jump-engage';
+Game.traits.Jump.prototype.EVENT_JUMP_CANCEL = 'jump-cancel';
+Game.traits.Jump.prototype.EVENT_JUMP_END    = 'jump-end';
 
 Game.traits.Jump.prototype.__obstruct = function(object, attack)
 {
@@ -57,10 +60,13 @@ Game.traits.Jump.prototype.engage = function()
     if (!host.isSupported) {
         return false;
     }
+
     host.isSupported = false;
     this._bump.copy(this.force);
     this._bump.x *= host.direction.x;
     this._elapsed = 0;
+
+    host.trigger(this.EVENT_JUMP_ENGAGE);
 }
 
 Game.traits.Jump.prototype.cancel = function()
@@ -68,6 +74,8 @@ Game.traits.Jump.prototype.cancel = function()
     if (this._elapsed !== undefined) {
         var progress = (this.duration - this._elapsed) / this.duration;
         this._host.physics.force.y -= this.force.y * progress;
+
+        this._host.trigger(this.EVENT_JUMP_CANCEL);
     }
     this._end();
 }
@@ -75,4 +83,5 @@ Game.traits.Jump.prototype.cancel = function()
 Game.traits.Jump.prototype._end = function()
 {
     this._elapsed = undefined;
+    this._host.trigger(this.EVENT_JUMP_END);
 }
