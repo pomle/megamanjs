a5bd79a0c244b1738706da3c680317a44f91bd57
Pontus Persson <pom@spotify.com>
2015-09-14 19:34:05 +0200

    Expose Marker.

--- src/tool/component/Editor.js
+++ src/tool/component/Editor.js
@@ -13,8 +13,10 @@ var Editor = function()
     this.items = new Editor.ItemSet();
 
     this.marker = new THREE.Mesh(
-        new THREE.SphereGeometry(5, 32, 32),
-        new THREE.MeshBasicMaterial({color: 0xffff00}));
+        new THREE.SphereGeometry(5, 2, 2),
+        new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true}));
+
+    this.modelManager = new Editor.ModelManager(this);
 
     this.workspace = $('<section class="workspace">');
     this.workspace.viewport = $('<div class="viewport">');
@@ -33,6 +35,25 @@ Editor.prototype.loadLevel = function(src, callback)
         loader = new Game.Loader.XML(game);
 
     loader.loadLevel(src, function(level, parser) {
+        editor.marker.position.set(0,0,0);
+
+        level.debug = true;
+        level.events.unbind(level.EVENT_START, level.resetPlayer);
+
+        level.camera.camera.far = 5000;
+        if (level.checkPoints.length) {
+            editor.marker.position.x = level.checkPoints[0].pos.x;
+            editor.marker.position.y = level.checkPoints[0].pos.y;
+
+            level.camera.jumpTo(level.checkPoints[0].pos);
+        }
+
+        game.engine.isSimulating = false;
+        game.setScene(level);
+        game.engine.world.updateTime(0);
+
+        editor.modelManager.expose(editor.marker);
+
         editor.document = parser.node;
         editor.document.object = editor.document.find('> objects');
         editor.document.layout = editor.document.find('> layout');
@@ -45,17 +66,6 @@ Editor.prototype.loadLevel = function(src, callback)
             editor.items.add(item);
         }
 
-        level.debug = true;
-        level.events.unbind(level.EVENT_START, level.resetPlayer);
-
-        level.camera.camera.far = 5000;
-        if (level.checkPoints.length) {
-            level.camera.jumpTo(level.checkPoints[0].pos);
-        }
-
-        game.engine.isSimulating = false;
-        game.setScene(level);
-        game.engine.world.updateTime(0);
 
         if (callback) {
             callback();
