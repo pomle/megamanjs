380d61172197e09ea18e547eb9e5fa72c15bb608
Pontus Persson <pom@spotify.com>
2015-10-24 10:08:03 +0200

    Micro optimize death zone trait.

--- src/game/traits/DeathZone.js
+++ src/game/traits/DeathZone.js
@@ -9,7 +9,7 @@ Game.traits.DeathZone.prototype.NAME = 'deathZone';
 
 Game.traits.DeathZone.prototype.__collides = function(withObject, ourZone, theirZone)
 {
-    if (withObject.health && !withObject.health.depleted) {
+    if (withObject.health !== undefined && withObject.health.depleted === false) {
         withObject.kill();
     }
 }
