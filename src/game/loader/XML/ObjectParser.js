642965b024dd44627102d03108a08480016e0298
Pontus Persson <pom@spotify.com>
2015-11-28 08:58:12 +0100

    Use MeshLambertMaterial for objects.

--- src/game/loader/XML/ObjectParser.js
+++ src/game/loader/XML/ObjectParser.js
@@ -173,7 +173,7 @@ Game.Loader.XML.Parser.ObjectParser.prototype.getObject = function(objectNode)
     {
         this.geometry = geometries[0].geometry.clone();
         if (this.textures.length) {
-            this.material = new THREE.MeshBasicMaterial({
+            this.material = new THREE.MeshLambertMaterial({
                 map: this.textures[0],
                 side: THREE.DoubleSide,
                 transparent: true,
