2ec09ee41ff2faf075113b1f0caebd7dcd317787
Pontus Persson <pom@spotify.com>
2016-01-25 22:04:16 +0100

    Remove appearingsolid obstacle.

--- src/game.js
+++ src/game.js
@@ -75,7 +75,6 @@ var Game = {
 
         "game/object/Solid.js",
         "game/object/Climbable.js",
-        "game/object/obstacle/AppearingSolid.js",
         "game/object/obstacle/DeathZone.js",
         "game/object/Character.js",
         "game/object/Item.js",
