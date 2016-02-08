2be5c2d14521bea92c097a111d48eb581c168011
Pontus Persson <pom@spotify.com>
2016-02-08 21:48:15 +0100

    Clean up math function formatting.

--- src/engine/Math.js
+++ src/engine/Math.js
@@ -100,21 +100,51 @@ Engine.Math.Geometry = {
  * only considering the t value for the range [0, 1] => [0, 1]
  */
 Engine.Easing = {
-  linear: function (t) { return t },
-  easeIn: function (p) { return function(t) { return Math.pow(t, p); } },
-  easeInQuad: function (t) { return t*t },
-  easeOutQuad: function (t) { return t*(2-t) },
-  easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
-  easeInCubic: function (t) { return t*t*t },
-  easeOutCubic: function (t) { return (--t)*t*t+1 },
-  easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
-  easeInQuart: function (t) { return t*t*t*t },
-  easeOutQuart: function (t) { return 1-(--t)*t*t*t },
-  easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
-  easeInQuint: function (t) { return t*t*t*t*t },
-  easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
-  easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t },
-  easeOutElastic: function(t) {
-    return Math.pow(2, -10 * t) * Math.sin((t - .375) * 20.93) + 1;
-  },
+    linear: function (t) {
+        return t
+    },
+    easeIn: function (p) {
+        return function(t) {
+            return Math.pow(t, p);
+        }
+    },
+    easeInQuad: function (t) {
+        return t*t
+    },
+    easeOutQuad: function (t) {
+        return t*(2-t)
+    },
+    easeInOutQuad: function (t) {
+        return t<.5 ? 2*t*t : -1+(4-2*t)*t
+    },
+    easeInCubic: function (t) {
+        return t*t*t
+    },
+    easeOutCubic: function (t) {
+        return (--t)*t*t+1
+    },
+    easeInOutCubic: function (t) {
+        return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1
+    },
+    easeInQuart: function (t) {
+        return t*t*t*t
+    },
+    easeOutQuart: function (t) {
+        return 1-(--t)*t*t*t
+    },
+    easeInOutQuart: function (t) {
+        return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t
+    },
+    easeInQuint: function (t) {
+        return t*t*t*t*t
+    },
+    easeOutQuint: function (t) {
+        return 1+(--t)*t*t*t*t
+    },
+    easeInOutQuint: function (t) {
+        return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t
+    },
+    easeOutElastic: function(t) {
+        return Math.pow(2, -10 * t) * Math.sin((t - .375) * 20.93) + 1;
+    },
 }
