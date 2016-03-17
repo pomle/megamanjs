485dd1f0a2037c44ad66e48d1c61f18a362111c6
Pontus Persson <pom@spotify.com>
2016-03-17 07:52:32 +0100

    Wrap prod.js in jQuery onready.

--- src/prod.js
+++ src/prod.js
@@ -1,24 +1,28 @@
-var game = Game.Loader.XML.createFromXML('resource/Megaman2.xml', function() {
-    game.attachToElement(document.getElementById('screen'));
-});
+$(function() {
+    var game = Game.Loader.XML.createFromXML('resource/Megaman2.xml', function() {
+        game.attachToElement(document.getElementById('screen'));
+    });
 
-var gameElement = document.getElementById('game');
+    var gameElement = document.getElementById('game');
 
-function on_fullscreen_change() {
-    if(document.mozFullScreen || document.webkitIsFullScreen) {
-        $(gameElement).addClass('fullscreen');
+    function on_fullscreen_change() {
+        if(document.mozFullScreen || document.webkitIsFullScreen) {
+            $(gameElement).addClass('fullscreen');
+        }
+        else {
+            $(gameElement).removeClass('fullscreen');
+        }
+
+        game.adjustAspectRatio();
     }
-    else {
-        $(gameElement).removeClass('fullscreen');
-    }
 
-    game.adjustAspectRatio();
-}
+    window.addEventListener('resize', on_fullscreen_change);
+    document.addEventListener('mozfullscreenchange', on_fullscreen_change);
+    document.addEventListener('webkitfullscreenchange', on_fullscreen_change);
 
-window.addEventListener('resize', on_fullscreen_change);
-document.addEventListener('mozfullscreenchange', on_fullscreen_change);
-document.addEventListener('webkitfullscreenchange', on_fullscreen_change);
+    $('button.fullscreen').on('click', function() {
+        gameElement.webkitRequestFullScreen();
+    });
 
-$('button.fullscreen').on('click', function() {
-    gameElement.webkitRequestFullScreen();
+    window.game = game;
 });
