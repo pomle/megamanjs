1fd115e9a0ac580f0286e2b24a99b6a86304963d
Pontus Persson <pom@spotify.com>
2016-03-17 02:24:31 +0100

    Remove old testsuite and adapt Timeline test.

---
+++ test/unit/tests/timeline.js
@@ -0,0 +1,56 @@
+var expect = require('expect.js');
+var sinon = require('sinon');
+
+var Timeline = require('../../importer.js').Engine.Timeline;
+
+describe('Timeline', function() {
+  it('should hold frame forever if not duration given', function() {
+    var anim = new Timeline();
+    anim.addFrame('A', 2);
+    anim.addFrame('B', 3);
+    anim.addFrame('C');
+    expect(isNaN(anim.totalDuration)).to.be(true);
+    expect(anim.getValueAtTime(1)).to.equal('A');
+    expect(anim.getValueAtTime(4)).to.equal('B');
+    expect(anim.getValueAtTime(6)).to.equal('C');
+    expect(anim.getValueAtTime(9)).to.equal('C');
+    expect(anim.getValueAtTime(12515152)).to.equal('C');
+  });
+  describe('#getLoopTime', function() {
+    it('should find correct place in time for any input number', function() {
+      var anim = new Timeline();
+      anim.addFrame({}, 1);
+      anim.addFrame({}, 4);
+      anim.addFrame({}, 2);
+      expect(anim.frames.length).to.equal(3);
+      expect(anim.totalDuration).to.equal(7);
+      expect(anim.accumulatedTime).to.equal(0);
+
+      var len = anim.totalDuration;
+      expect(anim.getLoopTime(-3)).to.equal(4);
+      expect(anim.getLoopTime(2)).to.equal(2);
+      // Big numbers
+      expect(anim.getLoopTime(2 + len * -4933)).to.equal(2);
+      expect(anim.getLoopTime(2 + len * 6823)).to.equal(2);
+      // Floats
+      expect(anim.getLoopTime(-3.2)).to.equal(3.8);
+      expect(anim.getLoopTime(6.3)).to.be.within(6.3, 6.300000001);
+    });
+  });
+  describe('#get(Index|Value)AtTime', function() {
+    it('should return the correct value or index given a time', function() {
+      var anim = new Timeline();
+      anim.addFrame('A', 4);
+      anim.addFrame('B', 2);
+      anim.addFrame('C', 3);
+      expect(anim.getIndexAtTime(0)).to.equal(0);
+      expect(anim.getValueAtTime(0)).to.equal('A');
+      expect(anim.getIndexAtTime(3)).to.equal(0);
+      expect(anim.getValueAtTime(3)).to.equal('A');
+      expect(anim.getIndexAtTime(4)).to.equal(1);
+      expect(anim.getValueAtTime(4)).to.equal('B');
+      expect(anim.getIndexAtTime(8.999)).to.equal(2);
+      expect(anim.getValueAtTime(8.999)).to.equal('C');
+    });
+  });
+});
