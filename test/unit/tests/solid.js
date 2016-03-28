8e103002dd22729963364d8dbfc4d224d74fa89e
Pontus Persson <pom@spotify.com>
2016-03-28 05:36:35 +0200

    Add test for Solid trait.

---
+++ test/unit/tests/solid.js
@@ -0,0 +1,40 @@
+var expect = require('expect.js');
+var sinon = require('sinon');
+
+var env = require('../../importer.js');
+
+var Obj = env.Engine.Object;
+var World = env.Engine.World;
+var Character = env.Game.objects.Character;
+var Physics = env.Game.traits.Physics;
+var Solid = env.Game.traits.Solid;
+
+describe('Trait', function() {
+  var step = 1/120;
+  describe('Solid', function() {
+    context('when supporting a character', function() {
+      it('should consistently provide ground support', function() {
+        var world = new World();
+        world.gravityForce.set(0, 10);
+        var ground = new Obj();
+        ground.applyTrait(new Solid());
+        ground.addCollisionRect(100, 10);
+        ground.position.set(0, 0, 0);
+        var actor = new Character();
+        actor.addCollisionRect(10, 10);
+        actor.position.set(0, 9, 0);
+        actor.applyTrait(new Solid());
+        actor.applyTrait(new Physics());
+        actor.physics.mass = 1;
+        world.addObject(actor);
+        world.addObject(ground);
+
+        var count = 0;
+        while (count++ < 120) {
+            world.updateTime(step);
+            expect(actor.position.y).to.equal(10);
+        }
+      });
+    });
+  });
+});
