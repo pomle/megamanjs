eb4b24bc334f7f59ec2b819f7661f5deb005a6df
Pontus Persson <pom@spotify.com>
2016-04-23 23:09:44 +0200

    Climtest

--- test/unit/tests/climbing.js
+++ test/unit/tests/climbing.js
@@ -7,7 +7,7 @@ describe('Climber / Climbable', function() {
   var climber;
   var climbable;
   var world;
-  before(function() {
+  beforeEach(function() {
     world = new env.Engine.World();
 
     climber = new env.Engine.Object();
@@ -17,14 +17,35 @@ describe('Climber / Climbable', function() {
 
     climbable = new env.Engine.Object();
     climbable.applyTrait(new env.Game.traits.Climbable());
-    climber.addCollisionRect(10, 100);
+    climbable.addCollisionRect(10, 10);
 
     world.addObject(climber);
     world.addObject(climbable);
   });
   context('when climber colliding with climbable', function() {
+    it('should be ignored when approaching from left', function() {
+      climber.position.set(-10, 0, 0);
+      climbable.position.set(0, 0, 0);
+      climber.velocity.set(10, 0);
+      world.updateTime(0.1);
+      expect(climber.position.x).to.be(-9.5);
+    });
+    it('should be ignored when approaching from right', function() {
+      climber.position.set(10, 0, 0);
+      climbable.position.set(0, 0, 0);
+      climber.velocity.set(-10, 0);
+      world.updateTime(0.1);
+      expect(climber.position.x).to.be(9.5);
+    });
+    it('should be ignored when approaching from bottom', function() {
+      climber.position.set(0, -10, 0);
+      climbable.position.set(0, 0, 0);
+      climber.velocity.set(0, 10);
+      world.updateTime(0.1);
+      expect(climber.position.y).to.be(-9.5);
+    });
     it('should be obstructed when approaching from above', function() {
-      climber.position.set(0, 80, 0);
+      climber.position.set(0, 10, 0);
       climbable.position.set(0, 0, 0);
       climber.velocity.set(0, -10);
       world.updateTime(0.1);
