d4d53ee2c565f8818bd56e47a2061dac9c105842
Pontus Persson <pom@spotify.com>
2016-04-29 17:49:30 -0400

    Classify Timeline.

--- src/engine/Timeline.js
+++ src/engine/Timeline.js
@@ -1,83 +1,78 @@
-Engine.Timeline = function()
-{
-    this.name = undefined;
+'use strict';
 
-    this.accumulatedTime = 0;
-    this.totalDuration = 0;
+Engine.Timeline = class Timeline {
+    constructor()
+    {
+        this.name = undefined;
 
-    this.frames = [];
-}
+        this.accumulatedTime = 0;
+        this.totalDuration = 0;
 
-Engine.Timeline.prototype.addFrame = function(value, duration)
-{
-    this.frames.push({
-        'duration': duration,
-        'value': value
-    });
-    this.totalDuration += duration;
-}
-
-Engine.Timeline.prototype.getIndex = function()
-{
-    return this.getIndexAtTime(this.accumulatedTime);
-}
-
-Engine.Timeline.prototype.getIndexAtTime = function(time)
-{
-    return this.resolveTime(time).index;
-}
-
-Engine.Timeline.prototype.getLoopTime = function(time)
-{
-    if (isFinite(this.totalDuration)) {
-        return (time % this.totalDuration + this.totalDuration) % this.totalDuration;
+        this.frames = [];
     }
-    return time;
-}
-
-Engine.Timeline.prototype.getValue = function()
-{
-    return this.resolveTime(this.accumulatedTime).value;
-}
-
-Engine.Timeline.prototype.getValueAtIndex = function(index)
-{
-    return this.frames[index].value;
-}
-
-Engine.Timeline.prototype.getValueAtTime = function(time)
-{
-    return this.resolveTime(time).value;
-}
-
-Engine.Timeline.prototype.resolveTime = function(time)
-{
-    /*
-        Because all JavaScript numbers are floats with finite precision
-        there's a chance this will crash because the accumulative durations
-        are less than infiniteTime.
-    */
-    time = this.getLoopTime(time);
-
-    var i = 0,
-        incrementalTime = 0,
-        index = 0;
-
-    do {
-        index = i++;
-        incrementalTime += this.frames[index].duration;
+    addFrame(value, duration)
+    {
+        this.frames.push({
+            'duration': duration,
+            'value': value
+        });
+        this.totalDuration += duration;
     }
-    while (time >= incrementalTime);
-
-    return {
-        index: index,
-        value: this.frames[index].value,
-        passedLength: incrementalTime - this.frames[index].duration,
-        resolvedLength: time,
+    getIndex()
+    {
+        return this.getIndexAtTime(this.accumulatedTime);
     }
-}
+    getIndexAtTime(time)
+    {
+        return this.resolveTime(time).index;
+    }
+    getLoopTime(time)
+    {
+        if (isFinite(this.totalDuration)) {
+            return (time % this.totalDuration + this.totalDuration) % this.totalDuration;
+        }
+        return time;
+    }
+    getValue()
+    {
+        return this.resolveTime(this.accumulatedTime).value;
+    }
+    getValueAtIndex(index)
+    {
+        return this.frames[index].value;
+    }
+    getValueAtTime(time)
+    {
+        return this.resolveTime(time).value;
+    }
+    resolveTime(time)
+    {
+        /*
+            Because all JavaScript numbers are floats with finite precision
+            there's a chance this will crash because the accumulative durations
+            are less than infiniteTime.
+        */
+        time = this.getLoopTime(time);
 
-Engine.Timeline.prototype.reset = function()
-{
-    this.accumulatedTime = 0;
+        let i = 0;
+        let incrementalTime = 0;
+        let index = 0;
+
+        do {
+            index = i++;
+            incrementalTime += this.frames[index].duration;
+        }
+        while (time >= incrementalTime);
+
+        return {
+            index: index,
+            value: this.frames[index].value,
+            passedLength: incrementalTime - this.frames[index].duration,
+            resolvedLength: time,
+        }
+    }
+    reset()
+    {
+        this.accumulatedTime = 0;
+    }
 }
