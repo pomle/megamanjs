4f92d99d8135ddf2339bbf8c2b19e3d74ea2626e
Pontus Persson <pom@spotify.com>
2016-04-29 17:42:31 -0400

    ES6ify UVCoords class.

--- src/engine/UVCoords.js
+++ src/engine/UVCoords.js
@@ -1,23 +1,25 @@
-Engine.UVCoords = function(offset, size, txSize)
-{
-    Array.call(this);
+'use strict';
 
-    var x = offset.x;
-    var y = offset.y;
-    var w = size.x;
-    var h = size.y;
-    var totalW = txSize.x;
-    var totalH = txSize.y;
+Engine.UVCoords = class UVCoords extends Array {
+    constructor(offset, size, txSize)
+    {
+        super();
 
-    var uvs = [
-        new THREE.Vector2(x / totalW, (totalH - y) / totalH),
-        new THREE.Vector2(x / totalW, (totalH - (y + h)) / totalH),
-        new THREE.Vector2((x + w) / totalW, (totalH - (y + h)) / totalH),
-        new THREE.Vector2((x + w) / totalW, (totalH - y) / totalH),
-    ];
+        const x = offset.x;
+        const y = offset.y;
+        const w = size.x;
+        const h = size.y;
+        const totalW = txSize.x;
+        const totalH = txSize.y;
 
-    this.push([uvs[0], uvs[1], uvs[3]],
-              [uvs[1], uvs[2], uvs[3]]);
+        const uvs = [
+            new THREE.Vector2(x / totalW, (totalH - y) / totalH),
+            new THREE.Vector2(x / totalW, (totalH - (y + h)) / totalH),
+            new THREE.Vector2((x + w) / totalW, (totalH - (y + h)) / totalH),
+            new THREE.Vector2((x + w) / totalW, (totalH - y) / totalH),
+        ];
+
+        this.push([uvs[0], uvs[1], uvs[3]],
+                  [uvs[1], uvs[2], uvs[3]]);
+    }
 }
-
-Engine.Util.extend(Engine.UVCoords, Array);
