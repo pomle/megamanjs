1e19cd3dd9ad21c0b5d9b170f727058af523b0a6
Pontus Persson <pom@spotify.com>
2016-05-04 15:14:19 -0400

    Replace grunt with custom build script.

---
+++ tools/concat-js.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const vm = require('vm');
+const fs = require('fs');
+const scripts = require('../src/script-manifest.json');
+
+let js = '';
+scripts.forEach((src) => {
+  js += fs.readFileSync(__dirname + '/../src/' + src, 'utf8') + '\n';
+});
+
+global.THREE = require('THREE');
+vm.runInThisContext(js);
+
+process.stdout.write(js);
