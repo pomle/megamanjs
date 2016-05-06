c3e133fe6fb86c564511abeaa8e3fd431979af5c
Pontus Persson <pom@spotify.com>
2016-05-06 10:41:06 -0700

    Code layout fixes.

--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 TOOLS_DIR=$(dirname $0)
 SRC_DIR="$(dirname $0)/../src"
 BUILD_DIR="$(dirname $0)/../build"
@@ -12,4 +13,3 @@ cp "${SRC_DIR}/prod.js" "${BUILD_DIR}/main.js"
 cp -r "${SRC_DIR}/game/resource" "${BUILD_DIR}/"
 
 node "${TOOLS_DIR}/concat-js.js" | cat > "${BUILD_DIR}/megaman.js"
-
