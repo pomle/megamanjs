4e4d02d37297a885d768f5c1e2fda738cd15f470
Pontus Persson <pom@spotify.com>
2016-06-07 16:52:30 +0200

    Add PositionalAudio class.

---
+++ src/engine/PositionalAudio.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Engine.PositionalAudio =
+class PositionalAudio
+extends Engine.Audio
+{
+    constructor(buffer, position)
+    {
+        super(buffer);
+        this._position = position;
+    }
+    getPosition()
+    {
+        return this._position;
+    }
+}
