02ef3b065ad93ccf391838964da4b29c0862b1ef
Pontus Persson <pom@spotify.com>
2016-06-08 22:11:30 +0200

    Move textureScale config property.

--- src/game/Loader.js
+++ src/game/Loader.js
@@ -3,4 +3,6 @@ Game.Loader = function(game)
     this.game = game;
     this.resourceManager = new Game.ResourceManager(this);
     this.resourceLoader = new Game.ResourceLoader(this);
+
+    this.textureScale = 1;
 }
