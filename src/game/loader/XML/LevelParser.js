12eb5211688efe9ba29d0c8db34c51ee5e439397
Pontus Persson <pom@spotify.com>
2016-06-13 21:05:49 +0200

    Deconsolidate SceneParser and LevelParser.

--- src/game/loader/XML/LevelParser.js
+++ src/game/loader/XML/LevelParser.js
@@ -18,29 +18,6 @@ extends Game.Loader.XML.SceneParser
 
         this._node = node;
         this._scene = new Game.scenes.Level();
-
-        this._bevahiorObjects = [];
-        this._layoutObjects = [];
-    }
-    getBehavior(node)
-    {
-        const type = node.parentNode.tagName.toLowerCase();
-        if (!this.BEHAVIOR_MAP[type]) {
-            throw new Error('Behavior ' + type + ' not in behavior map');
-        }
-        const constructor = this.BEHAVIOR_MAP[type];
-        const rect = this.getRect(node);
-        const instance = new constructor;
-        instance.addCollisionRect(rect.w, rect.h);
-        instance.position.x = rect.x;
-        instance.position.y = rect.y;
-        instance.position.z = 0;
-
-        return {
-            constructor: constructor,
-            instance: instance,
-            node: node,
-        };
     }
     _parse()
     {
@@ -69,42 +46,6 @@ extends Game.Loader.XML.SceneParser
             return this._scene;
         });
     }
-    _parseBehaviors()
-    {
-        const nodes = this._node.querySelectorAll(':scope > layout > behaviors > * > rect');
-        const world = this._scene.world;
-        for (let node, i = 0; node = nodes[i]; ++i) {
-            const object = this.getBehavior(node);
-            this._bevahiorObjects.push(object);
-            world.addObject(object.instance);
-        }
-        return Promise.resolve();
-    }
-    _parseCamera()
-    {
-        const cameraNode = this._node.querySelector(':scope > camera');
-        if (cameraNode) {
-            const camera = this._scene.camera;
-            const smoothing = this.getFloat(cameraNode, 'smoothing');
-            if (smoothing) {
-                camera.smoothing = smoothing;
-            }
-
-            const posNode = cameraNode.querySelector(':scope > position');
-            if (posNode) {
-                const position = this.getPosition(posNode);
-                camera.position.copy(position);
-            }
-
-            const pathNodes = cameraNode.querySelectorAll(':scope > path');
-            for (let pathNode, i = 0; pathNode = pathNodes[i]; ++i) {
-                const path = this.getCameraPath(pathNode);
-                camera.addPath(path);
-            }
-        }
-
-        return Promise.resolve();
-    }
     _parseCheckpoints()
     {
         const checkpointNodes = this._node.querySelectorAll(':scope > checkpoints > checkpoint');
@@ -113,15 +54,6 @@ extends Game.Loader.XML.SceneParser
             const p = this.getPosition(checkpointNode);
             const r = this.getFloat(checkpointNode, 'radius') || undefined;
             level.addCheckPoint(p.x, p.y, r);
-        }
-        return Promise.resolve();
-    }
-    _parseGravity()
-    {
-        const node = this._node.getElementsByTagName('gravity')[0];
-        if (node) {
-            const gravity = this.getVector2(node);
-            this._scene.world.gravityForce.copy(gravity);
         }
         return Promise.resolve();
     }
@@ -138,45 +70,6 @@ extends Game.Loader.XML.SceneParser
                 this.stopAudio(id);
             });
         }
-    }
-    _parseLayout()
-    {
-        const objectNodes = this._node.querySelectorAll(':scope > layout > objects > object');
-        const world = this._scene.world;
-        for (let objectNode, i = 0; objectNode = objectNodes[i]; ++i) {
-            const layoutObject = this._parseLayoutObject(objectNode);
-            world.addObject(layoutObject.instance);
-            this._layoutObjects.push(layoutObject);
-        };
-        return Promise.resolve();
-    }
-    _parseLayoutObject(node)
-    {
-        const objectId = node.getAttribute('id');
-        const object = this._getObject(objectId);
-        const instance = new object.constructor;
-        const position = this.getPosition(node) || this.DEFAULT_POS;
-        const scale = this.getFloat(node, 'scale') || 1;
-        instance.position.copy(position);
-        instance.model.scale.multiplyScalar(scale);
-
-        const traitNodes = node.getElementsByTagName('trait');
-        if (traitNodes) {
-            const traitParser = new Game.Loader.XML.TraitParser();
-            const traits = [];
-            for (let traitNode, i = 0; traitNode = traitNodes[i++];) {
-                const Trait = traitParser.parseTrait(traitNode);
-                const trait = new Trait;
-                instance.applyTrait(trait);
-            }
-        }
-
-        return {
-            sourceNode: object.node,
-            node: node,
-            constructor: object.constructor,
-            instance: instance,
-        };
     }
     _parseSpawners()
     {
--- src/game/loader/XML/SceneParser.js
+++ src/game/loader/XML/SceneParser.js
@@ -9,6 +9,28 @@ extends Game.Loader.XML.Parser
         super(loader);
         this._scene = scene;
         this._objects = {};
+        this._bevahiorObjects = [];
+        this._layoutObjects = [];
+    }
+    getBehavior(node)
+    {
+        const type = node.parentNode.tagName.toLowerCase();
+        if (!this.BEHAVIOR_MAP[type]) {
+            throw new Error('Behavior ' + type + ' not in behavior map');
+        }
+        const constructor = this.BEHAVIOR_MAP[type];
+        const rect = this.getRect(node);
+        const instance = new constructor;
+        instance.addCollisionRect(rect.w, rect.h);
+        instance.position.x = rect.x;
+        instance.position.y = rect.y;
+        instance.position.z = 0;
+
+        return {
+            constructor: constructor,
+            instance: instance,
+            node: node,
+        };
     }
     getScene()
     {
@@ -44,6 +66,42 @@ extends Game.Loader.XML.Parser
         }
         return Promise.all(tasks);
     }
+    _parseBehaviors()
+    {
+        const nodes = this._node.querySelectorAll(':scope > layout > behaviors > * > rect');
+        const world = this._scene.world;
+        for (let node, i = 0; node = nodes[i]; ++i) {
+            const object = this.getBehavior(node);
+            this._bevahiorObjects.push(object);
+            world.addObject(object.instance);
+        }
+        return Promise.resolve();
+    }
+    _parseCamera()
+    {
+        const cameraNode = this._node.querySelector(':scope > camera');
+        if (cameraNode) {
+            const camera = this._scene.camera;
+            const smoothing = this.getFloat(cameraNode, 'smoothing');
+            if (smoothing) {
+                camera.smoothing = smoothing;
+            }
+
+            const posNode = cameraNode.querySelector(':scope > position');
+            if (posNode) {
+                const position = this.getPosition(posNode);
+                camera.position.copy(position);
+            }
+
+            const pathNodes = cameraNode.querySelectorAll(':scope > path');
+            for (let pathNode, i = 0; pathNode = pathNodes[i]; ++i) {
+                const path = this.getCameraPath(pathNode);
+                camera.addPath(path);
+            }
+        }
+
+        return Promise.resolve();
+    }
     _parseEvents()
     {
         const node = this._node.querySelector(':scope > events');
@@ -58,6 +116,54 @@ extends Game.Loader.XML.Parser
                 scene.events.bind(event.name, event.callback);
             });
         });
+    }
+    _parseGravity()
+    {
+        const node = this._node.getElementsByTagName('gravity')[0];
+        if (node) {
+            const gravity = this.getVector2(node);
+            this._scene.world.gravityForce.copy(gravity);
+        }
+        return Promise.resolve();
+    }
+    _parseLayout()
+    {
+        const objectNodes = this._node.querySelectorAll(':scope > layout > objects > object');
+        const world = this._scene.world;
+        for (let objectNode, i = 0; objectNode = objectNodes[i]; ++i) {
+            const layoutObject = this._parseLayoutObject(objectNode);
+            world.addObject(layoutObject.instance);
+            this._layoutObjects.push(layoutObject);
+        };
+        return Promise.resolve();
+    }
+    _parseLayoutObject(node)
+    {
+        const objectId = node.getAttribute('id');
+        const object = this._getObject(objectId);
+        const instance = new object.constructor;
+        const position = this.getPosition(node) || this.DEFAULT_POS;
+        const scale = this.getFloat(node, 'scale') || 1;
+        instance.position.copy(position);
+        instance.model.scale.multiplyScalar(scale);
+
+        const traitNodes = node.getElementsByTagName('trait');
+        if (traitNodes) {
+            const traitParser = new Game.Loader.XML.TraitParser();
+            const traits = [];
+            for (let traitNode, i = 0; traitNode = traitNodes[i++];) {
+                const Trait = traitParser.parseTrait(traitNode);
+                const trait = new Trait;
+                instance.applyTrait(trait);
+            }
+        }
+
+        return {
+            sourceNode: object.node,
+            node: node,
+            constructor: object.constructor,
+            instance: instance,
+        };
     }
     _parseObjects()
     {
