8d6d9e19b67e9ee00aa6857d23a3683e99b0c6d7
Pontus Persson <pom@spotify.com>
2016-06-13 20:18:18 +0200

    Deconsolidate EventParser and ActionParser.

---
+++ src/game/loader/XML/ActionParser.js
@@ -0,0 +1,89 @@
+'use strict';
+
+Game.Loader.XML.ActionParser =
+class ActionParser
+extends Game.Loader.XML.Parser
+{
+    constructor()
+    {
+        super();
+    }
+    getAction(node)
+    {
+        const conditionNodes = node.querySelectorAll(':scope > condition');
+        const conditions = [];
+        for (let conditionNode, j = 0; conditionNode = conditionNodes[j++];) {
+            const value = this.getFloat(conditionNode, 'value') || this.getAttr(conditionNode, 'value');
+            conditions.push(value);
+        }
+
+        const callback = this._resolveFunction(node);
+
+        if (conditions.length > 0) {
+            const wrapper = function() {
+                for (let i = 0; i < conditions.length; ++i) {
+                    if (arguments[i] != conditions[i]) {
+                        return;
+                    }
+                }
+                callback.apply(this, arguments);
+            };
+            return wrapper;
+        }
+
+        return callback;
+    }
+    getEasing(node, attr)
+    {
+        const aggr = this.getAttr(node, attr).split(',');
+        const name = aggr.shift();
+        if (aggr.length) {
+            const val = parseFloat(aggr[0]);
+            return Engine.Easing[name](val);
+        } else {
+            return Engine.Easing[name];
+        }
+    }
+    _resolveFunction(node)
+    {
+        const type = this.getAttr(node, 'type');
+        const id = this.getAttr(node, 'id');
+
+        if (type === 'camera-move') {
+            const duration = this.getFloat(node, 'duration');
+            const easing = this.getEasing(node, 'easing');
+            const aggr = this.getAttr(node, 'to').split(',');
+            const to = new THREE.Vector3(parseFloat(aggr[0]) || undefined,
+                                         parseFloat(aggr[1]) || undefined,
+                                         parseFloat(aggr[2]) || undefined);
+            return function cameraTo() {
+                const c = this.camera.position;
+                const d = new THREE.Vector3(to.x || c.x, to.y || c.y, to.z || c.z);
+                return this.camera.panTo(d, duration, easing);
+            }
+        } else if (type === 'play-audio') {
+            return function playAudio() {
+                this.playAudio(id);
+            };
+        } else if (type === 'stop-audio') {
+            return function stopAudio() {
+                this.stopAudio(id);
+            };
+        } else if (type === 'emit-audio') {
+            return function emitAudio() {
+                this.world.emitAudio(this.audio[id]);
+            };
+        } else if (type === 'play-sequence') {
+            return function playSequence() {
+                return this.playSequence(id);
+            };
+        } else if (type === 'wait') {
+            const duration = this.getFloat(node, 'duration') || 0;
+            return function wait() {
+                return this.timer.waitFor(duration);
+            };
+        }
+
+        throw new Error(`No action "${type}"`);
+    }
+}
--- src/game/loader/XML/EventParser.js
+++ src/game/loader/XML/EventParser.js
@@ -24,61 +24,16 @@ extends Game.Loader.XML.Parser
     _parseEvents()
     {
         const events = [];
+        const parser = new Game.Loader.XML.ActionParser;
         const actionNodes = this._node.querySelectorAll(':scope > event > action');
         for (let actionNode, i = 0; actionNode = actionNodes[i++];) {
             const name = this.getAttr(actionNode.parentNode, 'name');
-            const action = this._parseAction(actionNode);
+            const action = parser.getAction(actionNode);
             events.push({
                 name: name,
                 callback: action,
             });
         }
         return events;
-    }
-    _parseAction(node)
-    {
-        const conditionNodes = node.querySelectorAll(':scope > condition');
-        const conditions = [];
-        for (let conditionNode, j = 0; conditionNode = conditionNodes[j++];) {
-            const value = this.getFloat(conditionNode, 'value') || this.getAttr(conditionNode, 'value');
-            conditions.push(value);
-        }
-
-        const callback = this._resolveFunction(node);
-
-        if (conditions.length > 0) {
-            const wrapper = function() {
-                for (let i = 0; i < conditions.length; ++i) {
-                    if (arguments[i] != conditions[i]) {
-                        return;
-                    }
-                }
-                callback.apply(this, arguments);
-            };
-            return wrapper;
-        }
-
-        return callback;
-    }
-    _resolveFunction(node)
-    {
-        const type = this.getAttr(node, 'type');
-        const id = this.getAttr(node, 'id');
-
-        if (type === 'play-audio') {
-            return function() {
-                this.playAudio(id);
-            };
-        } else if (type === 'stop-audio') {
-            return function() {
-                this.stopAudio(id);
-            };
-        } else if (type === 'emit-audio') {
-            return function() {
-                this.world.emitAudio(this.audio[id]);
-            };
-        }
-
-        throw new Error(`No action "${type}"`);
     }
 }
