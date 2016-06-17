9f7c29a80e272de70f924609bbb1a6b80a5b8085
Pontus Persson <pom@spotify.com>
2016-06-17 16:04:06 +0200

    Improve build script.
    * Error code on fail.
    * Echo build dir and OK.

--- tools/build.sh
+++ tools/build.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
+set -e
 
 TOOLS_DIR=$(dirname $0)
 SRC_DIR="$(dirname $0)/../src"
 BUILD_DIR="$(dirname $0)/../build"
+
+echo "Building to $(readlink -f $BUILD_DIR)"
 
 mkdir -p $BUILD_DIR
 
@@ -12,4 +15,6 @@ cp "${SRC_DIR}/prod.js" "${BUILD_DIR}/main.js"
 
 cp -r "${SRC_DIR}/resource" "${BUILD_DIR}/"
 
-node "${TOOLS_DIR}/concat-js.js" | cat > "${BUILD_DIR}/megaman.js"
+node "${TOOLS_DIR}/concat-js.js" > "${BUILD_DIR}/megaman.js"
+
+echo "Build OK"
