b6d13c6ec89f58a4cc9052295a79078e54fd99e7
Pontus Persson <pontus.alexander@gmail.com>
2016-07-03 22:23:54 +0200

    Strictify CanvasUtils.

--- src/engine/CanvasUtil.js
+++ src/engine/CanvasUtil.js
@@ -1,20 +1,22 @@
+'use strict';
+
 Engine.CanvasUtil = {
     clone: function(oldCanvas) {
-        var newCanvas = document.createElement('canvas');
-        var context = newCanvas.getContext('2d');
+        const newCanvas = document.createElement('canvas');
+        const context = newCanvas.getContext('2d');
         newCanvas.width = oldCanvas.width;
         newCanvas.height = oldCanvas.height;
         context.drawImage(oldCanvas, 0, 0);
         return newCanvas;
     },
     colorReplace: function(canvas, rgbIn, rgbOut) {
-        var context = canvas.getContext("2d");
-        var pixels = context.getImageData(0, 0, canvas.width, canvas.height);
-        var data = pixels.data;
-        for (var i = 0, l = data.length; i < l; i += 4) {
-            if (data[i] == rgbIn.x
-            && data[i+1] == rgbIn.y
-            && data[i+2] == rgbIn.z) {
+        const context = canvas.getContext("2d");
+        const pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+        const data = pixels.data;
+        for (let i = 0, l = data.length; i < l; i += 4) {
+            if (data[i] === rgbIn.x
+            && data[i+1] === rgbIn.y
+            && data[i+2] === rgbIn.z) {
                 data[i] = rgbOut.x;
                 data[i+1] = rgbOut.y;
                 data[i+2] = rgbOut.z;
@@ -24,12 +26,12 @@ Engine.CanvasUtil = {
         return canvas;
     },
     scale: function(canvas, scale) {
-        var x = canvas.width * scale;
-        var y = canvas.height * scale;
-        var newCanvas = document.createElement('canvas');
+        const x = canvas.width * scale;
+        const y = canvas.height * scale;
+        const newCanvas = document.createElement('canvas');
         newCanvas.width = x;
         newCanvas.height = y;
-        var context = newCanvas.getContext("2d");
+        const context = newCanvas.getContext("2d");
         context.imageSmoothingEnabled = scale < 1;
         context.drawImage(canvas, 0, 0, x, y);
         return newCanvas;
