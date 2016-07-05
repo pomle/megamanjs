674645656d202405b4887a2501da00b44fa3439e
Pontus Persson <pom@spotify.com>
2016-07-05 14:21:18 +0200

    Prevent character from being killed when dead.

--- src/game/object/Character.js
+++ src/game/object/Character.js
@@ -15,6 +15,10 @@ Game.objects.Character.prototype.EVENT_RESURRECT = 'resurrect';
 
 Game.objects.Character.prototype.kill = function()
 {
+    if (this.dead === true) {
+        return;
+    }
+
     this.dead = true;
     this.health.deplete();
 
@@ -29,6 +33,10 @@ Game.objects.Character.prototype.kill = function()
 
 Game.objects.Character.prototype.resurrect = function()
 {
+    if (this.dead === false) {
+        return;
+    }
+
     this.dead = false;
     this.health.fill();
     this.events.trigger(this.EVENT_RESURRECT);
