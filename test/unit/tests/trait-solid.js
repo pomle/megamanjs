5a7b32868694e230f4a75b68cdb3ce5b09271f69
Pontus Persson <pom@spotify.com>
2016-07-07 12:01:44 +0200

    Move solid trait obstruct test to integration suite.

--- test/unit/tests/trait-solid.js
+++ test/unit/tests/trait-solid.js
@@ -26,31 +26,4 @@ describe('Solid Trait', function() {
       expect(trait.obstructs).to.be(false);
     });
   });
-
-  context('when supporting a character', function() {
-    it('should consistently provide ground support', function() {
-      const world = new World();
-      world.gravityForce.set(0, 10);
-      const ground = new Obj();
-      ground.applyTrait(new Solid());
-      ground.solid.fixed = true;
-      ground.solid.obstructs = true;
-      ground.addCollisionRect(100, 10);
-      ground.position.set(0, 0, 0);
-      const actor = new Character();
-      actor.addCollisionRect(10, 10);
-      actor.position.set(0, 9, 0);
-      actor.applyTrait(new Solid());
-      actor.applyTrait(new Physics());
-      actor.physics.mass = 1;
-      world.addObject(actor);
-      world.addObject(ground);
-
-      let count = 0;
-      while (count++ < 120) {
-          world.updateTime(step);
-          expect(actor.position.y).to.equal(10);
-      }
-    });
-  });
 });
