f8a322cf95801925291a1394f338a5d5b4131872
Pontus Persson <pom@spotify.com>
2016-07-07 12:12:31 +0200

    Add unit test for Solid trait attackDirection().

--- test/unit/tests/trait-solid.js
+++ test/unit/tests/trait-solid.js
@@ -3,12 +3,9 @@
 const expect = require('expect.js');
 const sinon = require('sinon');
 
-const env = require('../../env.js');
+const env = require('../../env');
 
-const Obj = env.Engine.Object;
-const World = env.Engine.World;
-const Character = env.Game.objects.Character;
-const Physics = env.Game.traits.Physics;
+const Object = env.Engine.Object;
 const Solid = env.Game.traits.Solid;
 
 describe('Solid Trait', function() {
@@ -16,14 +13,46 @@ describe('Solid Trait', function() {
 
   context('when instantiating', function() {
     it('should have name set to "solid"', function() {
-      const trait = new Game.traits.Solid();
+      const trait = new Solid;
       expect(trait.NAME).to.be('solid');
     });
 
     it('should have fixed and obstructs set to false', function() {
-      const trait = new Game.traits.Solid();
+      const trait = new Solid;
       expect(trait.fixed).to.be(false);
       expect(trait.obstructs).to.be(false);
+    });
+  });
+
+  describe('#attackDirection()', function() {
+    const solid = new Solid;
+    const object1 = new Object;
+    const object2 = new Object;
+    object1.addCollisionRect(10, 10);
+    object2.addCollisionRect(10, 10);
+
+    it('should detect direction from above', function() {
+      object1.position.set(0, 0, 0);
+      object2.position.set(0, 9, 0);
+      expect(solid.attackDirection(object1.collision[0], object2.collision[0])).to.be(solid.TOP);
+    });
+
+    it('should detect direction from below', function() {
+      object1.position.set(0, 9, 0);
+      object2.position.set(0, 0, 0);
+      expect(solid.attackDirection(object1.collision[0], object2.collision[0])).to.be(solid.BOTTOM);
+    });
+
+    it('should detect direction from left', function() {
+      object1.position.set(9, 0, 0);
+      object2.position.set(0, 0, 0);
+      expect(solid.attackDirection(object1.collision[0], object2.collision[0])).to.be(solid.LEFT);
+    });
+
+    it('should detect direction from right', function() {
+      object1.position.set(0, 0, 0);
+      object2.position.set(9, 0, 0);
+      expect(solid.attackDirection(object1.collision[0], object2.collision[0])).to.be(solid.RIGHT);
     });
   });
 });
