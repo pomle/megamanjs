134ba574f2cd95c92b88253654c97326fdb8280a
Pontus Persson <pom@spotify.com>
2016-07-11 13:05:30 +0200

    Refactor Elevator trait and add tests.

--- src/game/traits/Elevator.js
+++ src/game/traits/Elevator.js
@@ -7,54 +7,64 @@ class Elevator extends Game.traits.Solid
 
         this.NAME = 'elevator';
 
-        this._timeline = new Engine.Timeline();
-        this._paths = [];
-        this._lastPos = new THREE.Vector2();
+        this._distance = 0;
+        this._nodes = [];
+        this._offset = new THREE.Vector2;
+        this._origo = null;
+        this._offset = new THREE.Vector2;
+        this._lastPos = new THREE.Vector2;
+        this._timeline = new Engine.Timeline;
+        this._velocity = new THREE.Vector2;
 
         this.fixed = true;
         this.obstructs = true;
-
         this.attackAccept = [this.TOP];
-        this.distance = 0;
-        this.origo = null;
-        this.offset = new THREE.Vector2();
-
         this.speed = 10;
+    }
+    __collides(subject, ourZone, theirZone)
+    {
+        const attack = super.__collides(subject, ourZone, theirZone);
+        if (attack === subject.SURFACE_TOP && subject.physics) {
+            subject.physics.velocity.copy(this._velocity);
+        }
     }
     __timeshift(deltaTime)
     {
-        this.distance += this.speed * deltaTime;
+        this._distance += this.speed * deltaTime;
 
-        const offset = this.resolvePosition(this.distance);
-        const p = this._host.position;
-        const z = p.z;
+        const offset = this.getOffset(this._distance);
+        const pos = this._host.position;
+        const z = pos.z;
 
-        if (this.origo === null) {
-            this.origo = p.clone();
+        if (!this._origo) {
+            this._origo = pos.clone();
         }
 
-        p.copy(this.origo).add(offset);
-        p.z = z;
+        pos.copy(this._origo).add(offset);
+        pos.z = z;
 
-        this._host.velocity.copy(p).sub(this._lastPos).divideScalar(deltaTime);
+        this._velocity.copy(pos)
+                      .sub(this._lastPos)
+                      .divideScalar(deltaTime);
 
-        this._lastPos.copy(p);
+        this._lastPos.copy(pos);
     }
     addNode(vec2)
     {
-        this._paths.push(vec2);
+        this._nodes.push(vec2);
         this._timeline.addFrame(vec2, vec2.length());
     }
-    resolvePosition(distance)
+    getOffset(distance)
     {
         const resolution = this._timeline.resolveTime(distance);
-        const offset = new THREE.Vector2();
+        const offset = this._offset;
+        offset.set(0, 0);
 
         for (let i = 0; i !== resolution.index; ++i) {
-            offset.add(this._paths[i]);
+            offset.add(this._nodes[i]);
         }
 
-        const pos = this._paths[resolution.index].clone();
+        const pos = this._nodes[resolution.index].clone();
         pos.setLength(resolution.resolvedLength - resolution.passedLength);
 
         offset.add(pos);
