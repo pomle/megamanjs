346ca396067d27b5b337eb5ec016bab86624b35e
Pontus Persson <pontus.alexander@gmail.com>
2016-07-12 19:38:58 +0200

    Refactor Translate trait and add tests.

--- src/game/traits/Translate.js
+++ src/game/traits/Translate.js
@@ -1,16 +1,16 @@
-Game.traits.Translate = function()
+Game.traits.Translate =
+class Translate extends Engine.Trait
 {
-    Engine.Trait.call(this);
-    this.velocity = new THREE.Vector2(1, 1);
-}
-
-Engine.Util.extend(Game.traits.Translate, Engine.Trait);
-
-Game.traits.Translate.prototype.NAME = 'translate';
-
-Game.traits.Translate.prototype.__timeshift = function(deltaTime)
-{
-    var host = this._host;
-    host.position.x += this.velocity.x * deltaTime;
-    host.position.y += this.velocity.y * deltaTime;
+    constructor()
+    {
+        super();
+        this.NAME = 'translate';
+        this.velocity = new THREE.Vector2(1, 1);
+    }
+    __timeshift(dt)
+    {
+        const pos = this._host.position;
+        pos.x += this.velocity.x * dt;
+        pos.y += this.velocity.y * dt;
+    }
 }
