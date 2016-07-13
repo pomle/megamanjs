e02861baf24a384294aa984b75a92212e2e3f960
Pontus Persson <pontus.alexander@gmail.com>
2016-07-13 18:55:40 +0200

    Decouple unit and integration tests.

---
+++ test/integration/tests/level.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const expect = require('expect.js');
+const sinon = require('sinon');
+
+const env = require('../../env');
+const AudioContextMock = require('../../mocks/audiocontext-mock');
+const WebGLRendererMock = require('../../mocks/webglrenderer-mock');
+const RequestAnimationFrameMock = require('../../mocks/requestanimationframe-mock');
+const Object = env.Engine.Object;
+const Level = env.Game.scenes.Level;
+
+describe('Level', function() {
+
+  function createLevel() {
+      AudioContextMock.mock();
+      WebGLRendererMock.mock();
+      RequestAnimationFrameMock.mock();
+      const level = new Level();
+      const game = new Game();
+      const character = new Object;
+      character.applyTrait(new Game.traits.Health);
+      game.player.setCharacter(character);
+      level.events.trigger(level.EVENT_CREATE, [game]);;
+      AudioContextMock.clean();
+      WebGLRendererMock.clean();
+      RequestAnimationFrameMock.clean();
+      return level;
+  }
+
+  it('should decrease player lives if player dies', function() {
+    const level = createLevel();
+    level.world.addObject(level.player.character);
+    level.player.lives = 3;
+    level.player.character.health.kill();
+    expect(level.player.lives).to.equal(2);
+  });
+
+  it('should run resetPlayer 4 seconds after death if lives > 1', function(done) {
+    const level = createLevel();
+    sinon.stub(level, 'resetPlayer', function() {
+      try {
+        expect(level.world._timeTotal).to.be(4);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+    level.world.addObject(level.player.character);
+    level.player.lives = 2;
+    level.player.character.health.kill();
+    level.world.updateTime(3.999);
+    level.world.updateTime(0.001);
+  });
+
+  it('should emit end event 4 seconds after death if lives <= 1', function() {
+    const level = createLevel();
+    const endEventSpy = sinon.spy(function() {
+      try {
+        expect(level.timer._timeTotal).to.be(4);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+    level.events.bind(level.EVENT_END, endEventSpy);
+    level.world.addObject(level.player.character);
+    level.player.lives = 1;
+    level.player.character.health.kill();
+    level.world.updateTime(3.999);
+    level.world.updateTime(0.001);
+  });
+});
