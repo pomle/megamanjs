8d339faef18a515817aa42629357e5226050767f
Pontus Persson <pom@spotify.com>
2016-07-15 18:55:48 +0200

    Refactor production bootstrap.

--- src/prod.js
+++ src/prod.js
@@ -4,9 +4,7 @@
 
     const gameElement = document.getElementById('game');
     const screenElement = document.getElementById('screen');
-
-    game.attachToElement(screenElement);
-    game.attachController(window);
+    const controlElement = screenElement.querySelector('.control');
 
     const progress = gameElement.querySelector('.progress-bar > .progress');
     loader.resourceLoader.events.bind(loader.resourceLoader.EVENT_PROGRESS, frac => {
@@ -17,12 +15,34 @@
         gameElement.classList.remove('busy');
     });
 
-    window.addEventListener('focus', function() {
-        game.resume();
-    });
-    window.addEventListener('blur', function() {
+    function pause() {
+        controlElement.classList.add('show');
         game.pause();
-    });
+    }
+
+    function resume() {
+        controlElement.classList.remove('show');
+        game.resume();
+    }
+
+    const actions = {
+        'fullscreen': e => {
+            gameElement.webkitRequestFullScreen();
+        },
+        'resume': e => {
+            resume();
+        },
+    }
+
+    function routeAction(e) {
+        const name = e.target.name;
+        for (const action in actions) {
+            if (name === action) {
+                actions[action](e);
+                return;
+            }
+        }
+    }
 
     function updateScreen() {
         const maxRes = {
@@ -41,6 +61,7 @@
         } else {
             size.w = bounds.width;
         }
+
         screenElement.style.width = size.w + 'px';
         screenElement.style.height = size.h + 'px';
         screenElement.style.marginTop = (bounds.height - size.h) / 2 + 'px';
@@ -50,13 +71,72 @@
                            Math.min(maxRes.y, size.h));
     }
 
+    screenElement.addEventListener('mousemove', pause);
+    document.addEventListener('click', routeAction);
+
+
     window.addEventListener('resize', updateScreen);
 
     loader.loadGame('./resource/Megaman2.xml').then(entrypoint => {
+        game.attachToElement(screenElement);
+        game.attachController(window);
+
         const hud = new Game.Hud;
         hud.attach(game, screenElement.querySelector('.energy'));
         updateScreen();
         gameElement.classList.add('ready');
+
+        const inputMapElement = controlElement.querySelector('.input-map');
+
+        inputMapElement.querySelector('#nes-controller').addEventListener('load', function(e) {
+            this.contentDocument.addEventListener('click', (function() {
+                let keyName = null;
+
+                const messageElement = inputMapElement.querySelector('.message');
+
+                let successTimeout;
+                function handleInputRemap(event) {
+                    const keyCode = event.keyCode;
+                    game.input.assign(keyCode, keyName);
+                    const res = messageElement.querySelector('#remap-success');
+                    res.innerHTML = res.dataset.text
+                        .replace('{{key}}', keyName)
+                        .replace('{{code}}', keyCode);
+                    messageElement.classList.add('success');
+                    clearTimeout(successTimeout);
+                    successTimeout = setTimeout(function() {
+                        messageElement.classList.remove('success');
+                    }, 2000);
+                    cancel();
+                }
+
+                function start() {
+                    const msg = messageElement.querySelector('#press-any-key');
+                    msg.innerHTML = msg.dataset.text.replace('{{key}}', keyName);
+                    messageElement.classList.add('listening');
+                    messageElement.classList.remove('success');
+                    game.input.disable();
+                    window.focus();
+                    window.addEventListener('keydown', handleInputRemap);
+                }
+
+                function cancel() {
+                    messageElement.classList.remove('listening');
+                    window.removeEventListener('keydown', handleInputRemap);
+                    game.input.enable();
+                }
+
+                return function handleInputRemap(event) {
+                    keyName = event.target.id;
+                    if (!keyName) {
+                        return;
+                    }
+                    cancel();
+                    start();
+                }
+            })());
+        });
+
         return loader.loadSceneByName(entrypoint);
     }).then(scene => {
         game.setScene(scene);
