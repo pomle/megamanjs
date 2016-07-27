205a62da029dc3496658ec2e3e5d1cfaba3db0e8
Pontus Persson <pom@spotify.com>
2016-07-27 19:35:19 +0200

    Test out the concept of a synchronous promise.

---
+++ src/engine/SyncPromise.js
@@ -0,0 +1,30 @@
+Engine.SyncPromise = class SyncPromise
+{
+    constructor()
+    {
+        this.PENDING = 0;
+        this.RESOLVED = 1;
+
+        this._chain = [];
+        this._state = this.PENDING;
+        this._value = null;
+    }
+    resolve(value)
+    {
+        if (this._state !== this.RESOLVED) {
+            this._value = value;
+            this._state = this.RESOLVED;
+            this._chain.forEach(callback => {
+                callback(value);
+            });
+        }
+    }
+    then(callback)
+    {
+        if (this._state === this.RESOLVED) {
+            callback(this._value);
+        } else {
+            this._chain.push(callback);
+        }
+    }
+}
