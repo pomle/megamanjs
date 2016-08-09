bd855a06ffd4ee48f73981786b0a6b1f5620ce30
Pontus Persson <pontus.alexander@gmail.com>
2016-08-09 22:46:24 +0200

    Fix Fallaway trait and add tests.

--- src/game/traits/Fallaway.js
+++ src/game/traits/Fallaway.js
@@ -2,8 +2,11 @@ Game.traits.Fallaway = function()
 {
     Engine.Trait.call(this);
 
-    this._countdown = undefined;
-    this.originalPosition = undefined;
+    this.__requires(Game.traits.Physics);
+
+    this._countdown = null;
+    this._originalPosition = null;
+
     this.delay = 1;
 }
 
@@ -19,31 +22,29 @@ Game.traits.Fallaway.prototype.__attach = function(host)
 
 Game.traits.Fallaway.prototype.__collides = function(withObject)
 {
-    if (this._countdown === undefined && withObject.isPlayer) {
+    if (this._countdown === null && withObject.isPlayer) {
         this._countdown = this.delay;
     }
 }
 
 Game.traits.Fallaway.prototype.__timeshift = function(deltaTime)
 {
-    if (this._countdown !== undefined) {
+    if (this._countdown !== null) {
+        this._countdown -= deltaTime;
         if (this._countdown <= 0) {
-            this.originalPosition = this._host.position.clone();
-            this._host.physics.enabled = true;
-            this._countdown = undefined;
-        }
-        else {
-            this._countdown -= deltaTime;
+            this._originalPosition = this._host.position.clone();
+            this._host.physics.enable();
+            this._countdown = null;
         }
     }
 }
 
 Game.traits.Fallaway.prototype.reset = function()
 {
-    this._host.physics.enabled = false;
+    this._host.physics.disable();
     this._host.physics.zero();
-    if (this.originalPosition) {
-        this._host.position.copy(this.originalPosition);
-        this.originalPosition = undefined;
+    if (this._originalPosition) {
+        this._host.position.copy(this._originalPosition);
+        this._originalPosition = null;
     }
 }
---
+++ test/unit/trait-fallaway.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const expect = require('expect.js');
+const sinon = require('sinon');
+
+const env = require('../env');
+const Object = env.Engine.Object;
+const Fallaway = env.Game.traits.Fallaway;
+const Physics = env.Game.traits.Physics;
+
+describe('Fallaway Trait', function() {
+  describe('on instantiation', function() {
+    let fallaway;
+    before(function() {
+       fallaway = new Fallaway;
+    });
+
+    it('has name "fallaway"', function() {
+      expect(fallaway.NAME).to.be('fallaway');
+    });
+
+    it('has delay set to 1', function() {
+      expect(fallaway.delay).to.be(1);
+    });
+
+    it('has a countdown set to null', function() {
+      expect(fallaway._countdown).to.be(null);
+    });
+  });
+
+  describe('when applied incorrectly', function() {
+    it('throws an expection if "Physics" trait not applied', function() {
+      const host = new Object;
+      expect(function() {
+        host.applyTrait(new Conveyor);
+      }).to.throwError();
+    });
+  });
+
+  describe('when applied successfully', function() {
+    let host;
+    before(function() {
+      host = new Object;
+      host.applyTrait(new Physics);
+      host.applyTrait(new Fallaway);
+      host.position.set(13, 17, -7);
+    });
+
+    it('exposes itself on host', function() {
+      expect(host.fallaway).to.be.a(Fallaway);
+    });
+
+    it('disables physics on host', function() {
+      expect(host.physics._enabled).to.be(false);
+    });
+
+    describe('when colliding with a player', function() {
+      let player;
+      before(function() {
+        player = new Object();
+        player.isPlayer = true;
+        host.collides(player);
+      });
+
+      it('starts its countdown with delay length', function() {
+        expect(host.fallaway._countdown).to.be(host.fallaway.delay);
+      });
+
+      describe('before countdown reaches 0', function() {
+        before(function() {
+          host.timeShift(.5);
+        });
+
+        it('physics is still disabled', function() {
+          expect(host.physics._enabled).to.be(false);
+        });
+      });
+
+      describe('when countdown reaches 0', function() {
+        before(function() {
+          host.timeShift(.6);
+        });
+
+        it('enables physics on host', function() {
+          expect(host.physics._enabled).to.be(true);
+        });
+
+        it('saves origin position', function() {
+          expect(host.position).to.eql(host.fallaway._originalPosition);
+        });
+
+        it('stops countdown', function() {
+          expect(host.fallaway._countdown).to.be(null);
+        });
+      });
+
+      describe('when reset', function() {
+        before(function() {
+          host.fallaway.reset();
+        });
+
+        it('disables physics on host', function() {
+          expect(host.physics._enabled).to.be(false);
+        });
+
+        it('puts host on origin', function() {
+          expect(host.position).to.eql({x: 13, y: 17, z: -7});
+        });
+
+        it('forgets origin', function() {
+          expect(host.fallaway._originalPosition).to.be(null);
+        });
+      });
+    });
+  });
+
+  describe('#reset()', function() {
+    let host
+    before(function() {
+      host = new Object;
+      host.applyTrait(new Physics);
+      host.applyTrait(new Fallaway);
+      sinon.stub(host.physics, 'disable');
+      sinon.stub(host.physics, 'zero');
+      host.reset();
+    });
+
+    it('runs disable() on physics trait', function() {
+      expect(host.physics.disable.calledOnce).to.be(true);
+    });
+
+    it('runs zero() on physics trait', function() {
+      expect(host.physics.zero.calledOnce).to.be(true);
+    });
+  });
+});
