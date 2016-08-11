2e56c82690f4f18ff5a9b1d741110ec907b6221c
Pontus Persson <pontus.alexander@gmail.com>
2016-08-11 21:41:46 +0200

    Update Fallaway trait to class.

--- src/game/traits/Fallaway.js
+++ src/game/traits/Fallaway.js
@@ -1,50 +1,46 @@
-Game.traits.Fallaway = function()
+Game.traits.Fallaway =
+class Fallaway extends Engine.Trait
 {
-    Engine.Trait.call(this);
+    constructor()
+    {
+        super();
+        this.NAME = 'fallaway';
+        this.__requires(Game.traits.Physics);
 
-    this.__requires(Game.traits.Physics);
+        this._countdown = null;
+        this._originalPosition = null;
 
-    this._countdown = null;
-    this._originalPosition = null;
-
-    this.delay = 1;
-}
-
-Engine.Util.extend(Game.traits.Fallaway, Engine.Trait);
-
-Game.traits.Fallaway.prototype.NAME = 'fallaway';
-
-Game.traits.Fallaway.prototype.__attach = function(host)
-{
-    Engine.Trait.prototype.__attach.call(this, host);
-    this.reset();
-}
-
-Game.traits.Fallaway.prototype.__collides = function(withObject)
-{
-    if (this._countdown === null && withObject.isPlayer) {
-        this._countdown = this.delay;
+        this.delay = 1;
     }
-}
-
-Game.traits.Fallaway.prototype.__timeshift = function(deltaTime)
-{
-    if (this._countdown !== null) {
-        this._countdown -= deltaTime;
-        if (this._countdown <= 0) {
-            this._originalPosition = this._host.position.clone();
-            this._host.physics.enable();
-            this._countdown = null;
+    __attach(host)
+    {
+        super.__attach(host);
+        this.reset();
+    }
+    __collides(withObject)
+    {
+        if (this._countdown === null && withObject.isPlayer) {
+            this._countdown = this.delay;
         }
     }
-}
-
-Game.traits.Fallaway.prototype.reset = function()
-{
-    this._host.physics.disable();
-    this._host.physics.zero();
-    if (this._originalPosition) {
-        this._host.position.copy(this._originalPosition);
-        this._originalPosition = null;
+    __timeshift(deltaTime)
+    {
+        if (this._countdown !== null) {
+            this._countdown -= deltaTime;
+            if (this._countdown <= 0) {
+                this._originalPosition = this._host.position.clone();
+                this._host.physics.enable();
+                this._countdown = null;
+            }
+        }
+    }
+    reset()
+    {
+        this._host.physics.disable();
+        this._host.physics.zero();
+        if (this._originalPosition) {
+            this._host.position.copy(this._originalPosition);
+            this._originalPosition = null;
+        }
     }
 }
