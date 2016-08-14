df028e82d0d67ab2fb23f0c2e6b34a0f7a0e8265
Pontus Persson <pom@spotify.com>
2016-08-14 19:09:05 +0200

    Decouple Browser test into Integration and System test.

--- tools/generate-browser-test-html.js
+++ tools/generate-browser-test-html.js
@@ -1,15 +1,56 @@
 #! /usr/bin/env node
-
 'use strict';
 
-const injectJS = require('./lib/inject').injectJS;
-
+const exec = require('child_process').exec;
 const fs = require('fs');
+const inject = require('./lib/inject').inject;
 
-let template = fs.readFileSync(__dirname + '/browser-test-template.html', 'utf8');
+function getDepScripts() {
+  const scripts = require('../src/script-manifest.json').map(src => '../../src/' + src);
+  scripts.unshift('../../src/lib/three.js');
+  return Promise.resolve(scripts);
+}
 
-const scripts = require('../src/script-manifest.json').map(src => '../../src/' + src);
-scripts.unshift('../../src/lib/three.js');
-template = injectJS('<!-- INJECT-JS-DEPS -->', template, scripts);
+function getTestFiles(path) {
+  const command = 'ls ' + path;
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout) => {
+      if (err) {
+        reject(err);
+      } else {
+        const files = stdout.split('\n')
+          .filter(item => item.length)
+          .map(item => './tests/' + item);
+        resolve(files);
+      }
+    });
+  });
+}
 
-process.stdout.write(template);
+function getTestTemplate() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(__dirname + '/browser-test-template.html', 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+Promise.all([
+  getTestTemplate(),
+  getTestFiles(process.argv[2]),
+  getDepScripts(),
+]).then(([template, tests, deps]) => {
+  template = inject('<!-- INJECT-JS-DEPS -->', template,
+    deps.map(src => '<script src="' + src + '"></script>'));
+
+  template = inject('<!-- INJECT-TEST-SCRIPTS -->', template,
+    tests.map(src => '<script src="' + src + '"></script>'));
+
+  process.stdout.write(template);
+}).catch(err => {
+  console.error(err);
+})
