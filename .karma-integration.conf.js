196ec88acb7ddbfadafce9076e492a7dcbf1b9f5
Pontus Persson <pom@spotify.com>
2016-08-16 13:49:00 +0200

    Run Karma with Mocha reporter.

--- .karma-integration.conf.js
+++ .karma-integration.conf.js
@@ -58,7 +58,8 @@ module.exports = function(_config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
-      'progress',
+      'mocha',
+      //'progress',
       //'coverage',
     ],
 
