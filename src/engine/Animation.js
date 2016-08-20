75b82175eeb1c348214a1a49ce07503f0289b219
Pontus Persson <pontus.alexander@gmail.com>
2016-08-20 20:49:21 +0200

    Clean up vectorTraverse function.

--- src/engine/Animation.js
+++ src/engine/Animation.js
@@ -1,18 +1,15 @@
 Engine.Animation = {
     units: ['x','y','z'],
 
-    vectorTraverse: function(subject, desired, speed)
-    {
-        var distance = 0;
-        var diff, axis;
-        for (var i in Engine.Animation.units) {
-            var axis = Engine.Animation.units[i];
+    vectorTraverse: (subject, desired, speed) => {
+        let distance = 0, diff, axis;
+        Engine.Animation.units.forEach(axis => {
             if (subject[axis] !== undefined && desired[axis] !== undefined) {
                 diff = Engine.Math.clamp(desired[axis] - subject[axis], -speed, speed);
                 subject[axis] += diff;
                 distance += Math.abs(subject[axis] - desired[axis]);
             }
-        }
+        });
         return distance;
-    }
+    },
 }
