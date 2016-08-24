06ea9cf6e3c2e29f07d172d4ffed3ab552d20551
Pontus Persson <pom@spotify.com>
2016-08-24 18:25:17 +0200

    Refactor Game system test.
    * Check player health.

--- test/system/tests/game.js
+++ test/system/tests/game.js
@@ -1,25 +1,42 @@
 'use strict';
 
 describe('Megaman 2', function() {
-  it('should load game with entrypoint Intro', function(done) {
-    const game = new Game;
-    const loader = new Game.Loader.XML(game);
-    loader.loadGame('/src/resource/Megaman2.xml').then(entrypoint => {
-      expect(entrypoint).to.be('Intro');
-      done();
-    });
-  });
-
-  it('should load a scene by name', function(done) {
-    const game = new Game;
-    const loader = new Game.Loader.XML(game);
-    loader.loadGame('/src/resource/Megaman2.xml')
-      .then(() => {
-        return loader.loadSceneByName('Intro');
-      })
-      .then(scene => {
-        expect(scene.name).to.be('Intro');
+  describe('when loading game XML', () => {
+    let game, loader, entrypoint;
+    before(done => {
+      game = new Game;
+      loader = new Game.Loader.XML(game);
+      loader.loadGame('/src/resource/Megaman2.xml').then(_entrypoint => {
+        entrypoint = _entrypoint;
         done();
       });
+    });
+
+    it('entrypoint is Intro', () => {
+      expect(entrypoint).to.be('Intro');
+    });
+
+    it('player has 28 health max', () => {
+      expect(game.player.character.health.energy.max).to.be(28);
+    });
+
+    it('player has 0 health min', () => {
+      expect(game.player.character.health.energy.min).to.be(0);
+    });
+
+    describe('Loader', () => {
+      it('should load a scene by name', function(done) {
+        const game = new Game;
+        const loader = new Game.Loader.XML(game);
+        loader.loadGame('/src/resource/Megaman2.xml')
+          .then(() => {
+            return loader.loadSceneByName('Intro');
+          })
+          .then(scene => {
+            expect(scene.name).to.be('Intro');
+            done();
+          });
+      });
+    });
   });
 });
