691d30886a5b3e05ed327604234e8395a0eca628
Pontus Persson <pom@spotify.com>
2016-08-24 17:24:47 +0200

    Classify and refactor Energy.

--- src/engine/logic/Energy.js
+++ src/engine/logic/Energy.js
@@ -1,111 +1,95 @@
-Engine.logic.Energy = function(max, min)
+Engine.logic.Energy = class Energy
 {
-    this.EVENT_CHANGE = 'change';
+    constructor(max = 100, min = 0)
+    {
+        this.EVENT_CHANGE = 'change';
 
-    this.events = new Engine.Events(this);
+        this.events = new Engine.Events(this);
 
-    this.max = max || 100;
-    this.min = min || 0;
-    this.amount = this.max;
-}
+        this._max = max;
+        this._min = min;
+        this._value = max;
 
-Object.defineProperties(Engine.logic.Energy.prototype, {
-    amount: {
-        enumerable: true,
-        get: function() {
-            return this._value;
-        },
-        set: function(v) {
-            if (this.infinite === true) {
-                return;
-            }
-            if (v === this._value) {
-                return;
-            }
-            if (!isFinite(v)) {
-                throw new TypeError('Value not a number');
-            }
-            if (v > this._max) {
-                this._value = this._max;
-            }
-            else if (v < this._min) {
-                this._value = this._min;
-            }
-            else {
-                this._value = v;
-            }
+        this.infinite = false;
+    }
+    get amount()
+    {
+        return this._value;
+    }
+    set amount(value)
+    {
+        if (!isFinite(value)) {
+            throw new TypeError('Value not a number');
+        }
 
+        const current = this._value;
+
+        if (this.infinite === true) {
+            return;
+        } else if (value > this._max) {
+            this._value = this._max;
+        } else if (value < this._min) {
+            this._value = this._min;
+        } else {
+            this._value = value;
+        }
+
+        if (current !== this._value) {
             this.events.trigger(this.EVENT_CHANGE);
-        },
-    },
-    depleted: {
-        enumerable: true,
-        get: function() {
-            return this._value <= this._min;
-        },
-    },
-    fraction: {
-        enumerable: true,
-        get: function() {
-            if (this.infinite === true || this._max === this._min) {
-                return 1;
-            }
-            else {
-                var total = Math.abs(this._max - this._min);
-                var rest = Math.abs(this._value - this._min);
-                return rest / total;
-            }
-        },
-    },
-    full: {
-        enumerable: true,
-        get: function() {
-            return this._value >= this._max;
-        },
-    },
-    infinite: {
-        enumerable: true,
-        writable: true,
-        value: false,
-    },
-    max: {
-        enumerable: true,
-        get: function() {
-            return this._max;
-        },
-        set: function(v) {
-            if (!isFinite(v)) {
-                throw new TypeError('Value not a number');
-            }
-            this._max = v;
-            if (this._max < this._value) {
-                this._value = this._max;
-            }
-        },
-    },
-    min: {
-        enumerable: true,
-        get: function() {
-            return this._min;
-        },
-        set: function(v) {
-            if (!isFinite(v)) {
-                throw new TypeError('Value not a number');
-            }
-            this._min = v;
-            if (this._min > this._value) {
-                this._value = this._min;
-            }
-        },
-    },
-});
-
-Engine.logic.Energy.prototype.deplete = function()
-{
-    this.amount = this._min;
-}
-
-Engine.logic.Energy.prototype.fill = function()
-{
-    this.amount = this._max;
+        }
+    }
+    deplete()
+    {
+        this.amount = this._min;
+    }
+    get depleted()
+    {
+        return this._value <= this._min;
+    }
+    get fraction()
+    {
+        if (this.infinite === true || this._max === this._min) {
+            return 1;
+        } else {
+            var total = Math.abs(this._max - this._min);
+            var rest = Math.abs(this._value - this._min);
+            return rest / total;
+        }
+    }
+    fill()
+    {
+        this.amount = this._max;
+    }
+    get full()
+    {
+        return this._value >= this._max;
+    }
+    get max()
+    {
+        return this._max;
+    }
+    set max(v)
+    {
+        if (!isFinite(v)) {
+            throw new TypeError('Value not a number');
+        }
+        this._max = v;
+        if (this._max < this._value) {
+            this.amount = this._max;
+        }
+    }
+    get min()
+    {
+        return this._min;
+    }
+    set min(v)
+    {
+        if (!isFinite(v)) {
+            throw new TypeError('Value not a number');
+        }
+        this._min = v;
+        if (this._min > this._value) {
+            this.amount = this._min;
+        }
+    }
 }
