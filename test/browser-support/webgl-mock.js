ea6b5cb93913f6b9f112d6801f7f5d2cf03809a5
Pontus Persson <pom@spotify.com>
2016-08-30 12:57:08 +0200

    Remove console.log from webgl-mock.

--- test/browser-support/webgl-mock.js
+++ test/browser-support/webgl-mock.js
@@ -1,10 +1,5 @@
 THREE.WebGLRenderer = function() {
-  console.debug('WebGLRenderer mock instantiated');
   this.domElement = document.createElement('canvas');
-  this.render = function() {
-    console.debug('WebGLRenderer.render()');
-  };
-  this.setSize = function() {
-    console.debug('WebGLRenderer.setSize()');
-  };
+  this.render = function() {}
+  this.setSize = function() {}
 }
