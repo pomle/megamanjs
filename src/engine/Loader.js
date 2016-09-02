2950a37d58a4dc8c658222994bcdcd2e643bb688
Pontus Persson <pontus.alexander@gmail.com>
2016-09-02 21:08:23 +0200

    Delete static engine files.

--- src/engine/Loader.js
+++
@@ -1,8 +0,0 @@
-Engine.Loader = function(game)
-{
-    this.game = game;
-    this.resourceManager = new Engine.ResourceManager(this);
-    this.resourceLoader = new Engine.ResourceLoader(this);
-
-    this.textureScale = 1;
-}
