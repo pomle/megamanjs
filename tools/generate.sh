cbb770fd89d74e84fc858a2b4b4297c2db53efde
Pontus Persson <pontus.alexander@gmail.com>
2017-02-05 21:45:23 -0800

    Rename dev.html to index.html.

--- tools/generate.sh
+++ tools/generate.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -e
 
-node ./tools/generate-dev-html.js > ./src/dev.html
+node ./tools/generate-dev-html.js > ./src/index.html
 node ./tools/generate-browser-test-html.js "test/integration/tests" > ./test/integration/index.html
 node ./tools/generate-browser-test-html.js "test/system/tests" > ./test/system/index.html
