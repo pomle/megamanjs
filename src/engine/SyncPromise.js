d4a2570a5fb9e625fee5744c58058621efd2fe60
Pontus Persson <pom@skurt.com>
2017-06-24 21:31:13 +0200

    Fix lint issues.

--- src/engine/SyncPromise.js
+++ src/engine/SyncPromise.js
@@ -3,28 +3,28 @@ class SyncPromise
     static all(tasks)
     {
         return new SyncPromise(resolve => {
-          let queue = tasks.length;
-          let values = [];
-          let index = 0;
+            let queue = tasks.length;
+            let values = [];
+            let index = 0;
 
-          function done(index, value) {
-              values[index] = value;
-              if (--queue === 0) {
-                  resolve(values);
-              }
-          }
-
-          tasks.forEach(task => {
-            values.push(null);
-            const i = index++;
-            if (task != null && typeof task.then === 'function') {
-              task.then(val => {
-                done(i, val);
-              });
-            } else {
-              done(i, task);
+            function done(index, value) {
+                values[index] = value;
+                if (--queue === 0) {
+                    resolve(values);
+                }
             }
-          });
+
+            tasks.forEach(task => {
+                values.push(null);
+                const i = index++;
+                if (task != null && typeof task.then === 'function') {
+                    task.then(val => {
+                        done(i, val);
+                    });
+                } else {
+                    done(i, task);
+                }
+            });
         });
     }
     static resolve(value = null)
@@ -40,38 +40,38 @@ class SyncPromise
         const deferred = [];
 
         function resolve(newValue) {
-          if(newValue && typeof newValue.then === 'function') {
-            newValue.then(resolve);
-            return;
-          }
-          value = newValue;
-          state = 'resolved';
+            if(newValue && typeof newValue.then === 'function') {
+                newValue.then(resolve);
+                return;
+            }
+            value = newValue;
+            state = 'resolved';
 
-          deferred.forEach(handle);
+            deferred.forEach(handle);
         }
 
         function handle(handler) {
-          if(state === 'pending') {
-            deferred.push(handler);
-            return;
-          }
+            if(state === 'pending') {
+                deferred.push(handler);
+                return;
+            }
 
-          if(!handler.onResolved) {
-            handler.resolve(value);
-            return;
-          }
+            if(!handler.onResolved) {
+                handler.resolve(value);
+                return;
+            }
 
-          const ret = handler.onResolved(value);
-          handler.resolve(ret);
+            const ret = handler.onResolved(value);
+            handler.resolve(ret);
         }
 
         this.then = function(onResolved) {
-          return new SyncPromise(resolve => {
-            handle({
-              onResolved: onResolved,
-              resolve: resolve,
+            return new SyncPromise(resolve => {
+                handle({
+                    onResolved: onResolved,
+                    resolve: resolve,
+                });
             });
-          });
         };
 
         fn(resolve);
