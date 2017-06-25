0273b755d1fe485fb000d46c860bbf56a685c027
Pontus Persson <pontus.alexander@gmail.com>
2017-06-26 01:49:12 +0200

    Use SnakeSilk packages from NPM.

--- src/engine.js
+++ src/engine.js
@@ -1,3 +1,3 @@
-window.Engine = require('snakesilk-engine');
-window.XMLLoader = require('snakesilk-xml-loader');
+window.Engine = require('@snakesilk/engine');
+window.XMLLoader = require('@snakesilk/xml-loader');
 window.THREE = require('three');
